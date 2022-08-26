mindula/ballerina-lang

Add resolve support

language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/BallerinaTextDocumentService.java
@@ -21,11 +21,15 @@ import io.ballerina.tools.text.LinePosition;
 import io.ballerina.tools.text.LineRange;
 import org.ballerinalang.formatter.core.Formatter;
 import org.ballerinalang.formatter.core.FormatterException;
+import org.ballerinalang.langserver.codeaction.providers.ResolvableCodeAction;
 import org.ballerinalang.langserver.codelenses.CodeLensUtil;
 import org.ballerinalang.langserver.codelenses.LSCodeLensesProviderHolder;
 import org.ballerinalang.langserver.common.utils.PathUtil;
 import org.ballerinalang.langserver.commons.BallerinaDefinitionContext;
 import org.ballerinalang.langserver.commons.CodeActionContext;
+import org.ballerinalang.langserver.commons.CodeActionContext;
+import org.ballerinalang.langserver.commons.CodeActionResolveContext;
+import org.ballerinalang.langserver.commons.CompletionContext;
 import org.ballerinalang.langserver.commons.CompletionContext;
 import org.ballerinalang.langserver.commons.DocumentServiceContext;
 import org.ballerinalang.langserver.commons.DocumentSymbolContext;
@@ -343,6 +347,18 @@ class BallerinaTextDocumentService implements TextDocumentService {
                         range.getStart(), range.getEnd());
             }
             return Collections.emptyList();
+        });
+    }
+
+    @Override
+    public CompletableFuture<CodeAction> resolveCodeAction(CodeAction codeAction) {
+        return CompletableFutures.computeAsync((cancelChecker) -> {
+            CodeActionResolveContext resolveContext = ContextBuilder.buildCodeActionResolveContext(
+                    workspaceManagerProxy.get(),
+                    this.serverContext,
+                    cancelChecker);
+            ResolvableCodeAction resolvableCodeAction = ResolvableCodeAction.from(codeAction);
+            return LangExtensionDelegator.instance().resolveCodeAction(resolvableCodeAction, resolveContext);
         });
     }
 

language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/LangExtensionDelegator.java
@@ -17,9 +17,11 @@
  */
 package org.ballerinalang.langserver;
 
+import org.ballerinalang.langserver.codeaction.providers.ResolvableCodeAction;
 import org.ballerinalang.langserver.common.utils.CommonUtil;
 import org.ballerinalang.langserver.commons.CodeActionContext;
 import org.ballerinalang.langserver.commons.CodeActionExtension;
+import org.ballerinalang.langserver.commons.CodeActionResolveContext;
 import org.ballerinalang.langserver.commons.CompletionContext;
 import org.ballerinalang.langserver.commons.CompletionExtension;
 import org.ballerinalang.langserver.commons.DiagnosticsExtension;
@@ -164,11 +166,30 @@ public class LangExtensionDelegator {
         for (CodeActionExtension ext : codeActionsExtensions) {
             if (this.handleURIScheme(uri, ext.handledCustomURISchemes(params, context, serverContext))
                     && ext.validate(params)) {
-                actions.addAll(ext.execute(params, context, serverContext));
+                List<? extends CodeAction> codeActions = ext.execute(params, context, serverContext);
+                for (CodeAction codeAction : codeActions) {
+                    ResolvableCodeAction action = ResolvableCodeAction.from(codeAction);
+                    if (codeAction instanceof ResolvableCodeAction) {
+                        action.getData().setExtName(ext.getClass().getName());
+                        actions.add(action);
+                    } else {
+                        actions.add(action);
+                    }
+                }
             }
         }
 
         return actions;
+    }
+
+    public CodeAction resolveCodeAction(ResolvableCodeAction resolvableCodeAction,
+                                        CodeActionResolveContext resolveContext) {
+        for (CodeActionExtension ext : codeActionsExtensions) {
+            if (ext.getClass().getName().equals(resolvableCodeAction.getData().getExtName())) {
+                return ext.resolve(resolvableCodeAction, resolveContext);
+            }
+        }
+        throw new IllegalStateException("Extension not found: " + resolvableCodeAction.getData().getExtName());
     }
 
     /**

language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/codeaction/CodeActionRouter.java
@@ -27,12 +27,16 @@ import io.ballerina.compiler.syntax.tree.SyntaxTree;
 import io.ballerina.tools.text.LinePosition;
 import org.ballerinalang.langserver.LSClientLogger;
 import org.ballerinalang.langserver.LSContextOperation;
+import org.ballerinalang.langserver.codeaction.providers.ResolvableCodeAction;
 import org.ballerinalang.langserver.common.utils.CommonUtil;
 import org.ballerinalang.langserver.common.utils.PositionUtil;
 import org.ballerinalang.langserver.common.utils.SymbolUtil;
 import org.ballerinalang.langserver.commons.CodeActionContext;
+import org.ballerinalang.langserver.commons.CodeActionResolveContext;
+import org.ballerinalang.langserver.commons.LanguageServerContext;
 import org.ballerinalang.langserver.commons.codeaction.CodeActionNodeType;
 import org.ballerinalang.langserver.commons.codeaction.spi.DiagBasedPositionDetails;
+import org.ballerinalang.langserver.commons.codeaction.spi.LSCodeActionProvider;
 import org.ballerinalang.langserver.commons.codeaction.spi.NodeBasedPositionDetails;
 import org.ballerinalang.langserver.telemetry.TelemetryUtil;
 import org.eclipse.lsp4j.CodeAction;
@@ -135,6 +139,20 @@ public class CodeActionRouter {
                             });
                 });
         return codeActions;
+    }
+
+    public static CodeAction resolveCodeAction(CodeAction codeAction,
+                                               CodeActionResolveContext resolveContext) {
+        CodeActionProvidersHolder codeActionProvidersHolder = CodeActionProvidersHolder
+                .getInstance(resolveContext.languageServercontext());
+        String codeActionName = ResolvableCodeAction.from(codeAction).getData().getCodeActionName();
+        Optional<LSCodeActionProvider> provider = codeActionProvidersHolder.getProviderByName(codeActionName);
+        CodeAction action = new CodeAction();
+        if (provider.isPresent()) {
+            action = provider.get().resolve(codeAction, resolveContext);
+        }
+
+        return action;
     }
 
     private static NonTerminalNode matchedStatementNode(CodeActionContext ctx, SyntaxTree syntaxTree) {

wso2/ballerina-vscode

packages/ballerina-language-server/language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/BallerinaTextDocumentService.java
@@ -26,6 +26,7 @@ import org.ballerinalang.langserver.codelenses.LSCodeLensesProviderHolder;
 import org.ballerinalang.langserver.common.utils.PathUtil;
 import org.ballerinalang.langserver.commons.BallerinaDefinitionContext;
 import org.ballerinalang.langserver.commons.CodeActionContext;
+import org.ballerinalang.langserver.commons.CodeActionResolveContext;
 import org.ballerinalang.langserver.commons.CompletionContext;
 import org.ballerinalang.langserver.commons.DocumentServiceContext;
 import org.ballerinalang.langserver.commons.DocumentSymbolContext;
@@ -38,6 +39,7 @@ import org.ballerinalang.langserver.commons.RenameContext;
 import org.ballerinalang.langserver.commons.SemanticTokensContext;
 import org.ballerinalang.langserver.commons.SignatureContext;
 import org.ballerinalang.langserver.commons.capability.LSClientCapabilities;
+import org.ballerinalang.langserver.commons.codeaction.ResolvableCodeAction;
 import org.ballerinalang.langserver.commons.eventsync.EventKind;
 import org.ballerinalang.langserver.contexts.ContextBuilder;
 import org.ballerinalang.langserver.definition.DefinitionUtil;
@@ -343,6 +345,30 @@ class BallerinaTextDocumentService implements TextDocumentService {
                         range.getStart(), range.getEnd());
             }
             return Collections.emptyList();
+        });
+    }
+
+    @Override
+    public CompletableFuture<CodeAction> resolveCodeAction(CodeAction codeAction) {
+        return CompletableFutures.computeAsync((cancelChecker) -> {
+            try {
+                CodeActionResolveContext resolveContext = ContextBuilder.buildCodeActionResolveContext(
+                        workspaceManagerProxy.get(),
+                        this.serverContext,
+                        cancelChecker);
+                ResolvableCodeAction resolvableCodeAction = ResolvableCodeAction.from(codeAction);
+                return LangExtensionDelegator.instance().resolveCodeAction(resolvableCodeAction, resolveContext);
+            } catch (UserErrorException e) {
+                this.clientLogger.notifyUser("Resolve Code Action", e);
+            } catch (CancellationException ignore) {
+                // Ignore the cancellation exception
+            } catch (Throwable e) {
+                String msg = "Operation 'text/resolveCodeAction' failed!";
+                this.clientLogger.logError(LSContextOperation.TXT_RESOLVE_CODE_ACTION, msg, e, null,
+                        (Position) null);
+            }
+
+            return null;
         });
     }
 

packages/ballerina-language-server/language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/LangExtensionDelegator.java
@@ -20,6 +20,7 @@ package org.ballerinalang.langserver;
 import org.ballerinalang.langserver.common.utils.CommonUtil;
 import org.ballerinalang.langserver.commons.CodeActionContext;
 import org.ballerinalang.langserver.commons.CodeActionExtension;
+import org.ballerinalang.langserver.commons.CodeActionResolveContext;
 import org.ballerinalang.langserver.commons.CompletionContext;
 import org.ballerinalang.langserver.commons.CompletionExtension;
 import org.ballerinalang.langserver.commons.DiagnosticsExtension;
@@ -27,6 +28,7 @@ import org.ballerinalang.langserver.commons.DocumentServiceContext;
 import org.ballerinalang.langserver.commons.FormattingExtension;
 import org.ballerinalang.langserver.commons.LanguageExtension;
 import org.ballerinalang.langserver.commons.LanguageServerContext;
+import org.ballerinalang.langserver.commons.codeaction.ResolvableCodeAction;
 import org.eclipse.lsp4j.CodeAction;
 import org.eclipse.lsp4j.CodeActionParams;
 import org.eclipse.lsp4j.CompletionItem;
@@ -164,11 +166,29 @@ public class LangExtensionDelegator {
         for (CodeActionExtension ext : codeActionsExtensions) {
             if (this.handleURIScheme(uri, ext.handledCustomURISchemes(params, context, serverContext))
                     && ext.validate(params)) {
-                actions.addAll(ext.execute(params, context, serverContext));
+                List<? extends CodeAction> codeActions = ext.execute(params, context, serverContext);
+                for (CodeAction codeAction : codeActions) {
+                    if (codeAction instanceof ResolvableCodeAction) {
+                        ResolvableCodeAction action = (ResolvableCodeAction) codeAction;
+                        action.getData().setExtName(ext.getClass().getName());
+                    }
+                    actions.add(codeAction);
+                }
             }
         }
 
         return actions;
+    }
+
+    public CodeAction resolveCodeAction(ResolvableCodeAction resolvableCodeAction,
+                                        CodeActionResolveContext resolveContext) {
+        for (CodeActionExtension ext : codeActionsExtensions) {
+            if (ext.getClass().getName().equals(resolvableCodeAction.getData().getExtName())) {
+                return ext.resolve(resolvableCodeAction, resolveContext);
+            }
+        }
+
+        throw new IllegalStateException("Extension not found: " + resolvableCodeAction.getData().getExtName());
     }
 
     /**

packages/ballerina-language-server/language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/codeaction/CodeActionRouter.java
@@ -28,7 +28,10 @@ import org.ballerinalang.langserver.LSContextOperation;
 import org.ballerinalang.langserver.common.utils.PositionUtil;
 import org.ballerinalang.langserver.common.utils.SymbolUtil;
 import org.ballerinalang.langserver.commons.CodeActionContext;
+import org.ballerinalang.langserver.commons.CodeActionResolveContext;
+import org.ballerinalang.langserver.commons.codeaction.ResolvableCodeAction;
 import org.ballerinalang.langserver.commons.codeaction.spi.DiagBasedPositionDetails;
+import org.ballerinalang.langserver.commons.codeaction.spi.LSCodeActionProvider;
 import org.ballerinalang.langserver.commons.codeaction.spi.RangeBasedPositionDetails;
 import org.ballerinalang.langserver.telemetry.TelemetryUtil;
 import org.eclipse.lsp4j.CodeAction;
@@ -133,6 +136,20 @@ public class CodeActionRouter {
                             });
                 });
         return codeActions;
+    }
+
+    public static CodeAction resolveCodeAction(ResolvableCodeAction codeAction,
+                                               CodeActionResolveContext resolveContext) {
+        CodeActionProvidersHolder codeActionProvidersHolder = CodeActionProvidersHolder
+                .getInstance(resolveContext.languageServercontext());
+        Optional<LSCodeActionProvider> provider = codeActionProvidersHolder.getProviderByName(
+                codeAction.getData().getCodeActionName());
+        CodeAction action = codeAction;
+        if (provider.isPresent()) {
+            action = provider.get().resolve(codeAction, resolveContext);
+        }
+
+        return action;
     }
 
     private static Optional<TypeSymbol> getMatchedTypeSymbol(CodeActionContext context, Node node) {