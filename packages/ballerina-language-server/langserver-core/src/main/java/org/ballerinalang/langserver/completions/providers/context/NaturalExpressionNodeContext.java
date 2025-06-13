6eda8983d16e1651bccc9e803f2c855e907c43ca
dulaj <dulaj@wso2.com>
2025-06-13 11:26:36 +0530

    Add NaturalExpressionNodeContext for completion suggestions in natural expressions

---
+++ packages/ballerina-language-server/langserver-core/src/main/java/org/ballerinalang/langserver/completions/providers/context/NaturalExpressionNodeContext.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ballerinalang.langserver.completions.providers.context;
+
+import io.ballerina.compiler.api.symbols.FunctionTypeSymbol;
+import io.ballerina.compiler.api.symbols.Symbol;
+import io.ballerina.compiler.api.symbols.SymbolKind;
+import io.ballerina.compiler.api.symbols.TypeDescKind;
+import io.ballerina.compiler.api.symbols.TypeSymbol;
+import io.ballerina.compiler.syntax.tree.InterpolationNode;
+import io.ballerina.compiler.syntax.tree.NaturalExpressionNode;
+import io.ballerina.compiler.syntax.tree.NonTerminalNode;
+import io.ballerina.compiler.syntax.tree.QualifiedNameReferenceNode;
+import io.ballerina.compiler.syntax.tree.SyntaxKind;
+import io.ballerina.compiler.syntax.tree.Token;
+import org.ballerinalang.annotation.JavaSPIService;
+import org.ballerinalang.langserver.common.utils.CommonUtil;
+import org.ballerinalang.langserver.common.utils.SymbolUtil;
+import org.ballerinalang.langserver.commons.BallerinaCompletionContext;
+import org.ballerinalang.langserver.commons.completion.LSCompletionItem;
+import org.ballerinalang.langserver.completions.SymbolCompletionItem;
+import org.ballerinalang.langserver.completions.providers.AbstractCompletionProvider;
+import org.ballerinalang.langserver.completions.util.QNameRefCompletionUtil;
+import org.ballerinalang.langserver.completions.util.SortingUtil;
+import org.wso2.ballerinalang.compiler.util.Names;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@JavaSPIService("org.ballerinalang.langserver.commons.completion.spi.BallerinaCompletionProvider")
+public class NaturalExpressionNodeContext extends AbstractCompletionProvider<NaturalExpressionNode> {
+
+    public NaturalExpressionNodeContext() {
+        super(NaturalExpressionNode.class);
+    }
+
+    @Override
+    public List<LSCompletionItem> getCompletions(BallerinaCompletionContext context, NaturalExpressionNode node) {
+        NonTerminalNode nodeAtCursor = context.getNodeAtCursor();
+        List<LSCompletionItem> completionItems = new ArrayList<>();
+
+        Optional<InterpolationNode> interpolationNode = findInterpolationNode(nodeAtCursor, node);
+        if (interpolationNode.isEmpty() || !this.isWithinInterpolation(context, node)) {
+            return completionItems;
+        }
+
+        if (QNameRefCompletionUtil.onQualifiedNameIdentifier(context, nodeAtCursor)) {
+            QualifiedNameReferenceNode qNameRef = (QualifiedNameReferenceNode) nodeAtCursor;
+            List<Symbol> moduleContent = QNameRefCompletionUtil.getModuleContent(
+                    context,
+                    qNameRef,
+                    this.getFunctionAndVariableFilterPredicate()
+            );
+            completionItems.addAll(this.getCompletionItemList(moduleContent, context));
+        } else {
+            completionItems.addAll(this.expressionCompletions(context));
+        }
+        SyntaxKind interpolationParent = interpolationNode.get().parent().kind();
+        this.sort(context, node, completionItems, interpolationParent);
+
+        return completionItems;
+    }
+
+    @Override
+    public boolean onPreValidation(BallerinaCompletionContext context, NaturalExpressionNode node) {
+        return node.textRange().startOffset() <= context.getCursorPositionInTree()
+                && context.getCursorPositionInTree() <= node.textRange().endOffset();
+    }
+
+    @Override
+    public void sort(BallerinaCompletionContext context,
+                     NaturalExpressionNode node,
+                     List<LSCompletionItem> completionItems,
+                     Object... interpolationParent) {
+        if (interpolationParent.length == 0 || !(interpolationParent[0] instanceof SyntaxKind)) {
+            throw new RuntimeException("Invalid sorting meta data provided");
+        }
+        /*
+        Sorting order will give the highest priority to the symbols.
+        Symbols which has a resolving type of boolean, int, float, decimal and string will get the highest priority.
+         */
+        for (LSCompletionItem lsCItem : completionItems) {
+            String sortText;
+            if (lsCItem.getType() != LSCompletionItem.CompletionItemType.SYMBOL
+                    || ((SymbolCompletionItem) lsCItem).getSymbol().isEmpty()) {
+                sortText = SortingUtil.genSortText(SortingUtil.toRank(context, lsCItem, 1));
+            } else {
+                Symbol symbol = ((SymbolCompletionItem) lsCItem).getSymbol().get();
+                Optional<TypeSymbol> typeSymbol = SymbolUtil.getTypeDescriptor(symbol);
+
+                sortText = typeSymbol.map(
+                        /*
+                        Here the sort text is three-fold.
+                        First we will assign the highest priority (Symbols over the others such as keywords),
+                        then we sort with the resolved type,
+                        Then we again append the sorting among the symbols (ex: functions over variable).
+                        */
+                                value -> SortingUtil.genSortText(1)
+                                        + this.getSortTextForResolvedType(value, (SyntaxKind) interpolationParent[0])
+                                        + SortingUtil.genSortText(SortingUtil.toRank(context, lsCItem))
+                        )
+                        .orElseGet(
+                                // Added for safety, and should not hit this point
+                                () -> SortingUtil.genSortText(SortingUtil.toRank(context, lsCItem, 1))
+                        );
+            }
+
+            lsCItem.getCompletionItem().setSortText(sortText);
+        }
+    }
+
+    /**
+     * Finds an {@link InterpolationNode} which is/is a parent of the cursor node.
+     *
+     * @param cursorNode Node at cursor
+     * @param node       Template expression node
+     * @return Optional interpolation node
+     */
+    private Optional<InterpolationNode> findInterpolationNode(NonTerminalNode cursorNode, NaturalExpressionNode node) {
+        // We know that the template expression node is definitely a parent of the node at the cursor
+        while (cursorNode.kind() != node.kind()) {
+            if (cursorNode.kind() == SyntaxKind.INTERPOLATION) {
+                return Optional.of((InterpolationNode) cursorNode);
+            }
+
+            cursorNode = cursorNode.parent();
+        }
+
+        return Optional.empty();
+    }
+
+    private boolean isWithinInterpolation(BallerinaCompletionContext context, NaturalExpressionNode node) {
+        NonTerminalNode nodeAtCursor = context.getNodeAtCursor();
+        Optional<InterpolationNode> interpolationNode = this.findInterpolationNode(nodeAtCursor, node);
+        int cursor = context.getCursorPositionInTree();
+        // Check if cursor is within the interpolation start and end tokens. Ex:
+        // 1. `some text ${..<cursor>..} other text`
+        if (interpolationNode.isEmpty()) {
+            return false;
+        }
+        Token startToken = interpolationNode.get().interpolationStartToken();
+        Token endToken = interpolationNode.get().interpolationEndToken();
+        return !startToken.isMissing() && startToken.textRange().endOffset() <= cursor
+                && (endToken.isMissing() || cursor <= endToken.textRange().startOffset());
+    }
+
+    private Predicate<Symbol> getFunctionAndVariableFilterPredicate() {
+        return CommonUtil.getVariableFilterPredicate()
+                .or(symbol -> symbol.kind() == SymbolKind.FUNCTION
+                        && !symbol.getName().orElse("").equals(Names.ERROR.getValue()));
+    }
+
+    private String getSortTextForResolvedType(TypeSymbol typeSymbol, SyntaxKind interpolationParent) {
+        TypeSymbol resolvedType = this.getResolvedType(typeSymbol);
+        TypeDescKind typeKind = resolvedType.typeKind();
+
+        // Note: The following logic can be simplified. Although, kept it as it is in order to improve the
+        // readability and maintainability over the changes
+        boolean booleanOrNumber = typeKind == TypeDescKind.BOOLEAN
+                || typeKind == TypeDescKind.INT
+                || typeKind == TypeDescKind.FLOAT
+                || typeKind == TypeDescKind.DECIMAL;
+        switch (interpolationParent) {
+            case STRING_TEMPLATE_EXPRESSION:
+                if (typeKind == TypeDescKind.BOOLEAN || typeKind == TypeDescKind.INT
+                        || typeKind == TypeDescKind.FLOAT || typeKind == TypeDescKind.DECIMAL
+                        || typeKind == TypeDescKind.STRING) {
+                    return SortingUtil.genSortText(1);
+                }
+                break;
+            case XML_ATTRIBUTE:
+                if (booleanOrNumber) {
+                    return SortingUtil.genSortText(1);
+                }
+                break;
+            case XML_ELEMENT:
+                if (typeKind == TypeDescKind.XML || typeKind == TypeDescKind.XML_COMMENT
+                        || typeKind == TypeDescKind.XML_ELEMENT || typeKind == TypeDescKind.XML_TEXT
+                        || typeKind == TypeDescKind.XML_PROCESSING_INSTRUCTION) {
+                    return SortingUtil.genSortText(1);
+                }
+                if (booleanOrNumber) {
+                    return SortingUtil.genSortText(2);
+                }
+                break;
+            default:
+                break;
+        }
+        return SortingUtil.genSortText(3);
+    }
+
+    private TypeSymbol getResolvedType(TypeSymbol typeSymbol) {
+        TypeSymbol resolvedType;
+        if (typeSymbol.typeKind() == TypeDescKind.FUNCTION) {
+            resolvedType = ((FunctionTypeSymbol) typeSymbol).returnTypeDescriptor().orElse(typeSymbol);
+        } else {
+            resolvedType = typeSymbol;
+        }
+
+        return CommonUtil.getRawType(resolvedType);
+    }
+}
