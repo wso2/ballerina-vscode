cd13225e830510415f35392e503123ed9ce926bc
rdulmina <rdulmina@gmail.com>
2025-06-25 10:06:33 +0530

    Fix issue where `ModuleNodeAnalyzer` does not populate function description properly

--- packages/ballerina-language-server/flow-model-generator/modules/flow-model-generator-core/src/main/java/io/ballerina/flowmodelgenerator/core/analyzers/function/ModuleNodeAnalyzer.java
+++ packages/ballerina-language-server/flow-model-generator/modules/flow-model-generator-core/src/main/java/io/ballerina/flowmodelgenerator/core/analyzers/function/ModuleNodeAnalyzer.java
@@ -330,11 +330,9 @@ public class ModuleNodeAnalyzer extends NodeVisitor {
         Map<String, String> params = new HashMap<>();
         String returnDescription = "";
         for (Node documentationLine : docNode.documentationLines()) {
-            if (documentationLine.kind() == SyntaxKind.MARKDOWN_DOCUMENTATION_LINE) {
+            if (isMarkdownDocumentationLine(documentationLine)) {
                 NodeList<Node> nodes = ((MarkdownDocumentationLineNode) documentationLine).documentElements();
-                if (nodes.size() == 1) {
-                    description.append(nodes.get(0).toSourceCode());
-                }
+                nodes.stream().forEach(node -> description.append(node.toSourceCode()));
             } else if (documentationLine.kind() == SyntaxKind.MARKDOWN_PARAMETER_DOCUMENTATION_LINE) {
                 MarkdownParameterDocumentationLineNode docLine =
                         (MarkdownParameterDocumentationLineNode) documentationLine;
@@ -353,6 +351,13 @@ public class ModuleNodeAnalyzer extends NodeVisitor {
             }
         }
         return new FunctionDocumentation(description.toString(), params, returnDescription);
+    }
+
+    private boolean isMarkdownDocumentationLine(Node node) {
+        SyntaxKind nodeKind = node.kind();
+        return nodeKind == SyntaxKind.MARKDOWN_DOCUMENTATION_LINE ||
+                nodeKind == SyntaxKind.MARKDOWN_REFERENCE_DOCUMENTATION_LINE ||
+                nodeKind == SyntaxKind.MARKDOWN_DEPRECATION_DOCUMENTATION_LINE;
     }
 
     private record FunctionDocumentation (String description, Map<String, String> parameterDescriptions,
