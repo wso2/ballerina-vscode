3ea740c45ed01b686cfae7e41a6c5ab9830a1ed5
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2025-07-03 09:29:00 +0530

    Change headers valueType

--- packages/ballerina-language-server/service-model-generator/modules/service-model-generator-ls-extension/src/main/java/io/ballerina/servicemodelgenerator/extension/model/HttpResponse.java
+++ packages/ballerina-language-server/service-model-generator/modules/service-model-generator-ls-extension/src/main/java/io/ballerina/servicemodelgenerator/extension/model/HttpResponse.java
@@ -62,7 +62,7 @@ public class HttpResponse {
         this.body = new Value(type, "TYPE", true);
         this.name = new Value("", "IDENTIFIER", true);
         this.type = new Value(type, "TYPE", true);
-        this.headers = new Value("", "MULTIPLE_SELECT", true);
+        this.headers = new Value("", "HEADER_SET", true);
     }
 
     public HttpResponse(String statusCode, String type, String body, Object headers, boolean editable) {
@@ -79,7 +79,7 @@ public class HttpResponse {
         Value body = new Value("", "EXPRESSION", true);
         Value name = new Value("", "EXPRESSION", true);
         Value type = new Value(typeStr, "EXPRESSION", true);
-        Value headers = new Value("", "MULTIPLE_SELECT", true);
+        Value headers = new Value("", "HEADER_SET", true);
         return new HttpResponse(statusCode, body, name, type, headers);
     }
 
