da078fc063e7a776504d21cf7190de47fdca5a3f
Nipuna Fernando <nipunafe@gmail.com>
2025-07-14 14:33:46 +0530

    Traverse the module members in the syntax tree

--- packages/ballerina-language-server/langserver-core/src/main/java/org/ballerinalang/langserver/codelenses/CodeLensUtil.java
+++ packages/ballerina-language-server/langserver-core/src/main/java/org/ballerinalang/langserver/codelenses/CodeLensUtil.java
@@ -13,15 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.ballerinalang.langserver.codelenses;
 
+import io.ballerina.compiler.syntax.tree.ModuleMemberDeclarationNode;
+import io.ballerina.compiler.syntax.tree.ModulePartNode;
+import io.ballerina.compiler.syntax.tree.NodeList;
+import io.ballerina.compiler.syntax.tree.SyntaxTree;
 import org.ballerinalang.langserver.commons.DocumentServiceContext;
 import org.ballerinalang.langserver.commons.codelenses.spi.LSCodeLensesProvider;
 import org.eclipse.lsp4j.CodeLens;
-import org.eclipse.lsp4j.TextDocumentIdentifier;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Provides code lenses related common functionalities.
@@ -39,14 +44,41 @@ public final class CodeLensUtil {
      * @param codeLensContext LSContext
      * @return a list of code lenses
      */
-    public static List<CodeLens> getCodeLenses(DocumentServiceContext codeLensContext, TextDocumentIdentifier txtDoc) {
+    public static List<CodeLens> getCodeLenses(DocumentServiceContext codeLensContext) {
         List<CodeLens> lenses = new ArrayList<>();
-        List<LSCodeLensesProvider> providers = LSCodeLensesProviderHolder
-                .getInstance(codeLensContext.languageServercontext()).getProviders();
-        for (LSCodeLensesProvider provider : providers) {
-            codeLensContext.checkCancelled();
-            lenses.addAll(provider.getLenses(codeLensContext));
+        Optional<SyntaxTree> syntaxTree = codeLensContext.currentSyntaxTree();
+        if (syntaxTree.isEmpty()) {
+            return lenses;
         }
+
+        // Check if the root node is a module part node
+        if (!syntaxTree.get().containsModulePart()) {
+            return lenses;
+        }
+
+        // Get all the code lenses providers
+        List<LSCodeLensesProvider> providers =
+                LSCodeLensesProviderHolder.getInstance(codeLensContext.languageServercontext()).getProviders();
+
+        // Get the module member nodes
+        // Note: The implementation is based on the assumption that all code lenses are attached to module-level
+        // types. If we need to support other scopes, A syntax tree visitor is needed instead of simply traversing
+        // the module members.
+        ModulePartNode modulePartNode = syntaxTree.get().rootNode();
+        NodeList<ModuleMemberDeclarationNode> members = modulePartNode.members();
+
+        // Traverse through the module members and collect code lenses
+        for (ModuleMemberDeclarationNode member : members) {
+            codeLensContext.checkCancelled();
+
+            // Validate the node with all the providers
+            for (LSCodeLensesProvider provider : providers) {
+                if (provider.validate(member)) {
+                    lenses.addAll(provider.getLenses(codeLensContext));
+                }
+            }
+        }
+
         return lenses;
     }
 }
