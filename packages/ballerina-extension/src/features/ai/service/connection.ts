9caa0c5d7d42ab37fa27ec160eb2adb593b26652
Anjana Supun <anjanasupun05@gmail.com>
2025-07-18 10:15:10 +0530

    Downgrade healthcare model

--- packages/ballerina-extension/src/features/ai/service/connection.ts
+++ packages/ballerina-extension/src/features/ai/service/connection.ts
@@ -18,7 +18,6 @@ export async function fetchWithAuth(input: string | URL | Request, options: Requ
         'Authorization': `Bearer ${accessToken}`,
         'User-Agent': 'Ballerina-VSCode-Plugin',
         'Connection': 'keep-alive',
-        // 'Keep-Alive': 'timeout=60, max=100'
     };
     
     let response = await fetch(input, options);
@@ -44,35 +43,13 @@ export async function fetchWithAuth(input: string | URL | Request, options: Requ
 }
 
 let url = "https://e95488c8-8511-4882-967f-ec3ae2a0f86f-prod.e1-us-east-azure.choreoapis.dev/ballerina-copilot/intelligence-api/v1.0/claude";
-// let url = "http://localhost:9090/intel/claude"
+
 export const anthropic = createAnthropic({
     baseURL: url,
     apiKey: "xx", //TODO: Gives error without this. see if we can remove,
     fetch: fetchWithAuth,
 });
 
-
-
 export const ANTHROPIC_HAIKU = "claude-3-5-haiku-20241022";
 export const ANTHROPIC_SONNET_4 = "claude-sonnet-4-20250514";
-
-//Components
-//TODO: Move libs into lang server API. - done
-//TODO: Host claude api with Auth - done
-//TODO: OpenAI Compatible API?
-//TODO: Token based throttling?
-
-//Auth
-//TODO: send oauth header / api-key 
-//TODO: use apiKey if BYOK
-
-//Migrations
-//TODO: Migrate healthcare
-//TODO: Evals?
-
-// Why?
-// As our 80% our customer base dont wanna use wso2 managed platform, we need to provide either self hosting capabilities or BYOK. BYOK is way easier for these customers. 
-// Earlier, we had opininated API for the backend, but when features evolving, maintaining that API is hard while keeping backward compatibility. ex - changing llm response format without breaking frontend. 
-// Agent mode, Local mcp tools needs the tools to be executed in local machine, either we'll have to go with websockets or to go with complex imlp
-// As all logic is contained in ballerina-extension module, can reduce the code complexity by reducing RPC calls by more than 70%. Easier to test the full flow without relying on other variables, state management. 
-
+export const ANTHROPIC_SONNET_3_5 = "claude-3-5-sonnet-20241022";
