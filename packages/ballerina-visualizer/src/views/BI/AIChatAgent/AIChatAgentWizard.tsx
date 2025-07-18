93941684ac3e5533e378b5171b1bd48622d22c04
Dan Niles <niles_dan@live.com>
2025-07-18 11:33:40 +0530

    Refactor AIChatAgentWizard to generate agent and model code

--- packages/ballerina-visualizer/src/views/BI/AIChatAgent/AIChatAgentWizard.tsx
+++ packages/ballerina-visualizer/src/views/BI/AIChatAgent/AIChatAgentWizard.tsx
@@ -17,16 +17,18 @@
  */
 
 import { useEffect, useRef, useState } from 'react';
-import { EVENT_TYPE, ListenerModel } from '@wso2/ballerina-core';
+import { CodeData, EVENT_TYPE, FlowNode, LinePosition, ListenerModel } from '@wso2/ballerina-core';
 import { View, ViewContent, TextField, Button, Typography } from '@wso2/ui-toolkit';
 import styled from '@emotion/styled';
 import { useRpcContext } from '@wso2/ballerina-rpc-client';
+import { URI, Utils } from "vscode-uri";
 import { LoadingContainer } from '../../styles';
 import { TitleBar } from '../../../components/TitleBar';
 import { TopNavigationBar } from '../../../components/TopNavigationBar';
 import { RelativeLoader } from '../../../components/RelativeLoader';
 import { FormHeader } from '../../../components/FormHeader';
 import { getAgentOrg } from './utils';
+import { cloneDeep } from 'lodash';
 
 const FORM_WIDTH = 600;
 
@@ -70,6 +72,8 @@ export function AIChatAgentWizard(props: AIChatAgentWizardProps) {
     const [listenerModel, setListenerModel] = useState<ListenerModel>(undefined);
     const [isCreating, setIsCreating] = useState<boolean>(false);
     const [currentStep, setCurrentStep] = useState<number>(0);
+    const [agentNode, setAgentNode] = useState<FlowNode | null>(null);
+    const [defaultModelNode, setDefaultModelNode] = useState<FlowNode | null>(null);
     const steps = [
         { label: "Creating Agent", description: "Creating the AI chat agent" },
         { label: "Initializing", description: "Setting up the agent configuration" },
@@ -79,9 +83,32 @@ export function AIChatAgentWizard(props: AIChatAgentWizardProps) {
         { label: "Completing", description: "Finalizing the agent setup" }
     ];
 
+    const agentFilePath = useRef<string>("");
     const agentOrg = useRef<string>("");
+    const agentCallEndOfFile = useRef<LinePosition | null>(null);
+    const agentEndOfFile = useRef<LinePosition | null>(null);
+    const mainFilePath = useRef<string>("");
+
     const initWizard = async () => {
+        // get agent file path
+        const filePath = await rpcClient.getVisualizerLocation();
+        agentFilePath.current = Utils.joinPath(URI.file(filePath.projectUri), "agents.bal").fsPath;
+        // get agent org
         agentOrg.current = await getAgentOrg(rpcClient);
+        // fetch agent node
+        await fetchAgentNode();
+        // get end of files
+        // main.bal last line
+        mainFilePath.current = Utils.joinPath(URI.file(filePath.projectUri), "main.bal").fsPath;
+        const endOfFile = await rpcClient.getBIDiagramRpcClient().getEndOfFile({ filePath: mainFilePath.current });
+        console.log(">>> endOfFile", endOfFile);
+        agentCallEndOfFile.current = endOfFile;
+        // agent.bal file last line
+        const endOfAgentFile = await rpcClient
+            .getBIDiagramRpcClient()
+            .getEndOfFile({ filePath: agentFilePath.current });
+        console.log(">>> endOfAgentFile", endOfAgentFile);
+        agentEndOfFile.current = endOfAgentFile;
     }
 
     useEffect(() => {
@@ -94,6 +121,49 @@ export function AIChatAgentWizard(props: AIChatAgentWizardProps) {
             setNameError("Failed to initialize the wizard. Please try again.");
         });
     }, []);
+
+    const getNodeTemplate = async (
+        codeData: CodeData,
+        filePath: string,
+        position: LinePosition = { line: 0, offset: 0 }
+    ) => {
+        const response = await rpcClient.getBIDiagramRpcClient().getNodeTemplate({
+            position: position,
+            filePath: filePath,
+            id: codeData,
+        });
+        console.log(">>> get node template response", response);
+        return response?.flowNode;
+    };
+
+    const fetchAgentNode = async () => {
+        // get the agent node
+        const allAgents = await rpcClient.getAIAgentRpcClient().getAllAgents({ filePath: agentFilePath.current, orgName: agentOrg.current });
+        console.log(">>> allAgents", allAgents);
+        if (!allAgents.agents.length) {
+            console.log(">>> no agents found");
+            return;
+        }
+        const agentCodeData = allAgents.agents.at(0);
+        // get agent node template
+        const agentNodeTemplate = await getNodeTemplate(agentCodeData, agentFilePath.current);
+        setAgentNode(agentNodeTemplate);
+
+        // get all llm models
+        const allModels = await rpcClient
+            .getAIAgentRpcClient()
+            .getAllModels({ agent: agentCodeData.object, filePath: agentFilePath.current, orgName: agentOrg.current });
+        console.log(">>> allModels", allModels);
+        // get openai model
+        const defaultModel = allModels.models.find((model) => model.object === "OpenAiProvider" || (model.org === "ballerina" && model.module === "ai"));
+        if (!defaultModel) {
+            console.log(">>> no default model found");
+            return;
+        }
+        // get model node template
+        const modelNodeTemplate = await getNodeTemplate(defaultModel, agentFilePath.current);
+        setDefaultModelNode(modelNodeTemplate);
+    };
 
     const validateName = (name: string): boolean => {
         if (!name) {
@@ -151,9 +221,41 @@ export function AIChatAgentWizard(props: AIChatAgentWizardProps) {
                 rpcClient.getServiceDesignerRpcClient().addServiceSourceCode({
                     filePath: "",
                     service: res.service
-                }).then((sourceCode) => {
+                }).then(async (sourceCode) => {
                     setCurrentStep(4);
                     const newArtifact = sourceCode.artifacts.find(res => res.isNew);
+                    console.log(">>> agent service sourceCode", sourceCode);
+                    console.log(">>> newArtifact", newArtifact);
+                    // save model node
+                    const modelResponse = await rpcClient
+                        .getBIDiagramRpcClient()
+                        .getSourceCode({ filePath: agentFilePath.current, flowNode: defaultModelNode });
+                    console.log(">>> modelResponse getSourceCode", { modelResponse });
+                    const modelVarName = defaultModelNode.properties.variable.value as string;
+
+                    // wait 2 seconds (wait until LS is updated)
+                    console.log(">>> wait 2 seconds");
+                    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+                    // save the agent node
+                    const updatedAgentNode = cloneDeep(agentNode);
+                    const systemPromptValue = `{role: "", instructions: string \`\`}`;
+                    const agentVarName = `_${agentName}Agent`;
+                    updatedAgentNode.properties.systemPrompt.value = systemPromptValue;
+                    updatedAgentNode.properties.model.value = modelVarName;
+                    updatedAgentNode.properties.tools.value = [];
+                    updatedAgentNode.properties.verbose.value = "false";
+                    updatedAgentNode.properties.maxIter.value = "0";
+                    updatedAgentNode.properties.variable.value = agentVarName;
+
+                    const agentResponse = await rpcClient
+                        .getBIDiagramRpcClient()
+                        .getSourceCode({ filePath: agentFilePath.current, flowNode: updatedAgentNode });
+                    console.log(">>> agentResponse getSourceCode", { agentResponse });
+
+                    // wait 2 seconds (wait until LS is updated)
+                    console.log(">>> wait 2 seconds");
+                    await new Promise((resolve) => setTimeout(resolve, 2000));
                     if (newArtifact) {
                         setCurrentStep(5);
                         rpcClient.getVisualizerRpcClient().openView({ type: EVENT_TYPE.OPEN_VIEW, location: { documentUri: newArtifact.path, position: newArtifact.position } });
