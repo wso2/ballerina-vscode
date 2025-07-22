206d5946323b5dd30f839bec6dd3dfa0793effda
Nuvindu Nirmana <63797478+Nuvindu@users.noreply.github.com>
2025-07-22 19:43:43 +0530

    Fix review suggestions
    Co-authored-by: Nipuna Fernando <59343084+nipunayf@users.noreply.github.com>

--- packages/ballerina-language-server/flow-model-generator/modules/flow-model-generator-ls-extension/src/test/java/io/ballerina/flowmodelgenerator/extension/agentsmanager/FlowNodeGeneratorTest.java
+++ packages/ballerina-language-server/flow-model-generator/modules/flow-model-generator-ls-extension/src/test/java/io/ballerina/flowmodelgenerator/extension/agentsmanager/FlowNodeGeneratorTest.java
@@ -76,8 +76,8 @@ public class FlowNodeGeneratorTest extends AbstractLSTest {
 
         boolean flowEquality = modifiedDiagram.equals(testConfig.diagram());
         if (!fileNameEquality || !flowEquality) {
-//            TestConfig updatedConfig = new TestConfig(testConfig.start(), testConfig.end(), testConfig.source(),
-//                    testConfig.description(), modifiedDiagram);
+            TestConfig updatedConfig = new TestConfig(testConfig.start(), testConfig.end(), testConfig.source(),
+                    testConfig.description(), modifiedDiagram);
 //            updateConfig(configJsonPath, updatedConfig);
             compareJsonElements(modifiedDiagram, testConfig.diagram());
             Assert.fail(String.format("Failed test: '%s' (%s)", testConfig.description(), configJsonPath));
