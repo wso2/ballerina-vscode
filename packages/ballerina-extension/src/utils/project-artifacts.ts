20fcfae4fcb9a560420ae1be37fdd66f56dd169b
Anjana S Porawagama <axewilledge123@gmail.com>
2025-07-23 16:50:21 +0530

    Refactor project artifact update logic to improve directory checks and enhance clarity

--- packages/ballerina-extension/src/utils/project-artifacts.ts
+++ packages/ballerina-extension/src/utils/project-artifacts.ts
@@ -59,15 +59,9 @@ export async function buildProjectArtifactsStructure(projectDir: string, langCli
 export async function updateProjectArtifacts(publishedArtifacts: ArtifactsNotification): Promise<void> {
     // Current project structure
     const currentProjectStructure: ProjectStructureResponse = StateMachine.context().projectStructure;
-    if (publishedArtifacts && currentProjectStructure) {
-        const tmpUri = URI.file(tmpdir());
-        const projectUri = URI.parse(StateMachine.context().projectUri);
-        const publishedArtifactsUri = URI.parse(publishedArtifacts.uri);
-        if (!projectUri.path.toLowerCase().includes(tmpUri.path.toLowerCase()) && publishedArtifactsUri.path.toLowerCase().includes(tmpUri.path.toLowerCase())) {
-            // Skip the temp dirs
-            console.log('Skipping temp dirs', publishedArtifactsUri.path, tmpUri.path);
-            return;
-        }
+    const projectUri = URI.parse(StateMachine.context().projectUri);
+    const isWithinProject = URI.parse(publishedArtifacts.uri).path.toLowerCase().includes(projectUri.path.toLowerCase());
+    if (currentProjectStructure && isWithinProject) {
         const entryLocations = await traverseUpdatedComponents(publishedArtifacts.artifacts, currentProjectStructure);
         if (entryLocations.length > 0) {
             const notificationHandler = ArtifactNotificationHandler.getInstance();
