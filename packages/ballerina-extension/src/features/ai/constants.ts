67c451a1b02da736e8f6bb9b865bb9519952d01b
Dan Niles <niles_dan@live.com>
2025-07-28 14:51:39 +0530

    Refactor error handling for  `Configure default model provider` command

--- packages/ballerina-extension/src/features/ai/constants.ts
+++ packages/ballerina-extension/src/features/ai/constants.ts
@@ -17,6 +17,11 @@
  */
 
 export const CONFIG_FILE_NAME = "Config.toml";
-export const PROGRESS_BAR_MESSAGE_FROM_WSO2_DEFAULT_MODEL = "Fetching and saving access token for WSO2 default model";
+export const CONFIGURE_DEFAULT_MODEL_COMMAND = "ballerina.configureWso2DefaultModelProvider";
+export const OPEN_AI_PANEL_COMMAND = "ballerina.open.ai.panel";
+export const SIGN_IN_BI_COPILOT = "Sign in to BI Copilot";
+export const PROGRESS_BAR_MESSAGE_FROM_WSO2_DEFAULT_MODEL = "Fetching and saving access token for WSO2 default model provider.";
 export const ERROR_NO_BALLERINA_SOURCES = "No Ballerina sources";
 export const LOGIN_REQUIRED_WARNING = "Please sign in to BI Copilot to use this feature.";
+export const LOGIN_REQUIRED_WARNING_FOR_DEFAULT_MODEL = "Please sign in to BI Copilot to configure the WSO2 default model provider.";
+export const DEFAULT_PROVIDER_ADDED = "WSO2 default model provider configuration values were added to the Config.toml file.";
