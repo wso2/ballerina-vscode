19037c9d57d447f057b661ed07948a2f64908a98
RNViththagan <viththagan1999@gmail.com>
2025-08-05 14:24:08 +0530

    Adds copyright header and minor code cleanup
    Adds WSO2 Apache License copyright header to maintain legal compliance.
    
    Removes extra blank line and adds clarifying comment for error handling case.
    Marks diagnostic fix response variable for potential refactoring review.

--- packages/ballerina-extension/src/features/ai/service/code/code.ts
+++ packages/ballerina-extension/src/features/ai/service/code/code.ts
@@ -1,3 +1,19 @@
+// Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com/) All Rights Reserved.
+
+// WSO2 LLC. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 import { CoreMessage, generateText, streamText } from "ai";
 import { getAnthropicClient, ANTHROPIC_SONNET_4 } from "../connection";
 import { GenerationType, getAllLibraries } from "../libs/libs";
@@ -66,7 +82,6 @@ ${getSystemPromptSuffix(LANGLIBS)}`,
         LibraryProviderTool: getLibraryProviderTool(libraryDescriptions, GenerationType.CODE_GENERATION),
     };
 
-
     const { fullStream } = streamText({
         model: await getAnthropicClient(ANTHROPIC_SONNET_4),
         maxTokens: 4096 * 4,
@@ -97,6 +112,7 @@ ${getSystemPromptSuffix(LANGLIBS)}`,
                 const finishReason = part.finishReason;
                 console.log("Finish reason: ", finishReason);
                 if (finishReason === "error") {
+                    // Already handled in error case.
                     break;
                 }
                 const postProcessedResp: PostProcessResponse = await postProcess({
@@ -107,7 +123,7 @@ ${getSystemPromptSuffix(LANGLIBS)}`,
 
                 const MAX_REPAIR_ATTEMPTS = 3;
                 let repair_attempt = 0;
-                let diagnosticFixResp = assistantResponse;
+                let diagnosticFixResp = assistantResponse; //TODO: Check if we need this variable
                 while (
                     hasCodeBlocks(diagnosticFixResp) &&
                     diagnostics.length > 0 &&
