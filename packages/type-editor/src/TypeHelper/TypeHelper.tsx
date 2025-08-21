58d73671a07496871525660708a6f9f0d82a4d2a
Senith Uthsara <senithkarunarathneu@gmail.com>
2025-08-21 11:32:37 +0530

    move type creation to a modal

--- packages/type-editor/src/TypeHelper/TypeHelper.tsx
+++ packages/type-editor/src/TypeHelper/TypeHelper.tsx
@@ -243,7 +243,7 @@ export const TypeHelperComponent = (props: TypeHelperComponentProps) => {
     return (
         <HelperPaneCustom >
             <SlidingWindow>
-                <HelperPane.Body>
+                <HelperPane.Body sx={{padding: '10px'}}>
                     <SearchBox sx={{ width: "100%", marginBottom: '10px' }} placeholder='Search' value={searchValue} onChange={handleHelperPaneSearch} />
                     {loading ? (
                         <HelperPane.Loader rows={3} columns={1} sections={3} />
