f84645fb3a8c7d577969b1fa1a6512fa2c53809d
Senith Uthsara <senithkarunarathneu@gmail.com>
2025-08-26 11:52:13 +0530

    give same exp as library browser in dynamic modal

--- packages/ballerina-visualizer/src/views/BI/HelperPaneNew/Components/Modal.tsx
+++ packages/ballerina-visualizer/src/views/BI/HelperPaneNew/Components/Modal.tsx
@@ -1,7 +1,7 @@
 import React, { useState, cloneElement, isValidElement, ReactNode, ReactElement, useEffect } from "react";
 import { createPortal } from "react-dom";
 import styled from "@emotion/styled";
-import { Codicon, Divider, ThemeColors } from "@wso2/ui-toolkit";
+import { Codicon, Divider, ThemeColors, Typography } from "@wso2/ui-toolkit";
 
 export type DynamicModalProps = {
     children: ReactNode;
@@ -11,31 +11,43 @@ export type DynamicModalProps = {
     width?: number;
     height?: number;
     openState: boolean;
-    setOpenState: (state: boolean)=>void;
+    setOpenState: (state: boolean) => void;
 };
 
+const ModalContainer = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 30000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
 const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  z-index: 30000 !important;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: var(--vscode-editor-inactiveSelectionBackground);
+    opacity: 0.4;
 `;
 
 const ModalBox = styled.div<{ width?: number; height?: number }>`
   width: ${({ width }: { width?: number }) => (width ? `${width}px` : 'auto')};
   height: ${({ height }: { height?: number }) => (height ? `${height}px` : 'auto')};
-  background-color: ${ThemeColors.PRIMARY_CONTAINER};
   position: relative;
-  padding: 10px;
   display: flex;
   flex-direction: column;
   overflow-y: hidden;
+  padding: 16px;
+  border-radius: 8px;
+  background-color: ${ThemeColors.PRIMARY_CONTAINER};
+  box-shadow: 0 3px 8px rgb(0 0 0 / 0.2);
+    z-index: 30001;
 `;
 
 const InvisibleButton = styled.button`
@@ -60,6 +72,13 @@ const Title = styled.h1`
     margin-left: 20px;
     margin-top: 10px;
     margin-bottom: 10px;
+`;
+
+const ModalHeaderSection = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-inline: 16px;
 `;
 
 type TriggerProps = React.ButtonHTMLAttributes<HTMLButtonElement> & { children: ReactNode };
@@ -94,7 +113,7 @@ const DynamicModal: React.FC<DynamicModalProps> & { Trigger: typeof Trigger } = 
         onClose && onClose();
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log("inside")
         setOpenState(openState)
     }, [openState]);
@@ -103,16 +122,19 @@ const DynamicModal: React.FC<DynamicModalProps> & { Trigger: typeof Trigger } = 
         <>
             {trigger}
             {openState && createPortal(
-                <Overlay className="unq-modal-overlay" ref={anchorRef}>
+                <ModalContainer className="unq-modal-overlay">
+                    <Overlay onClick={handleClose} />
                     <ModalBox width={width} height={height}>
-                        <InvisibleButton onClick={handleClose} style={{ position: "absolute", top: 8, right: 8 }}>
-                            <Codicon name="close" />
-                        </InvisibleButton>
-                        <Title>{title}</Title>
-                        <Divider/>
+                        <ModalHeaderSection>
+                            <Typography variant="h2" sx={{ margin: 0}}>
+                                {title}
+                            </Typography>
+                            <Codicon name="close" onClick={handleClose} />
+                        </ModalHeaderSection>
+                        <Divider />
                         <div>{content}</div>
                     </ModalBox>
-                </Overlay>,
+                </ModalContainer>,
                 document.body
             )}
         </>
