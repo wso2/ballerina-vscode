4ae37eeb467ed60c8d7df469cc73827f57d1a320
nipunayf <nipunafe@gmail.com>
2025-09-08 23:22:51 +0530

    Implement a subscriber for resolving compilation errors

---
+++ packages/ballerina-language-server/langserver-core/src/main/java/org/ballerinalang/langserver/eventsync/subscribers/ResolveCompilationErrorsSubscriber.java
@@ -0,0 +1,79 @@
+/*
+ *  Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com)
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.ballerinalang.langserver.eventsync.subscribers;
+
+import org.ballerinalang.annotation.JavaSPIService;
+import org.ballerinalang.compiler.BLangCompilerException;
+import org.ballerinalang.langserver.command.executors.PullModuleExecutor;
+import org.ballerinalang.langserver.commons.DocumentServiceContext;
+import org.ballerinalang.langserver.commons.LanguageServerContext;
+import org.ballerinalang.langserver.commons.client.ExtendedLanguageClient;
+import org.ballerinalang.langserver.commons.eventsync.EventKind;
+import org.ballerinalang.langserver.commons.eventsync.spi.EventSubscriber;
+import org.eclipse.lsp4j.MessageActionItem;
+import org.eclipse.lsp4j.MessageType;
+import org.eclipse.lsp4j.ShowMessageRequestParams;
+
+import java.util.List;
+
+/**
+ * Subscriber to popup notification and resolve compilation errors on project updates.
+ *
+ * @since 1.2.0
+ */
+@JavaSPIService("org.ballerinalang.langserver.commons.eventsync.spi.EventSubscriber")
+public class ResolveCompilationErrorsSubscriber implements EventSubscriber {
+
+    public static final String NAME = "Resolve compilation errors subscriber";
+    private static final String PULL_MODULES_ACTION = "Pull Modules";
+
+    @Override
+    public EventKind eventKind() {
+        return EventKind.PROJECT_UPDATE;
+    }
+
+    @Override
+    public void onEvent(ExtendedLanguageClient client, DocumentServiceContext context,
+                        LanguageServerContext serverContext) {
+        try {
+            context.workspace().waitAndGetPackageCompilation(context.filePath());
+        } catch (BLangCompilerException e) {
+            String message = e.getMessage();
+            if (message != null && message.startsWith("failed to load the module")) {
+                ShowMessageRequestParams showMessageRequestParams = new ShowMessageRequestParams();
+                showMessageRequestParams.setType(MessageType.Error);
+                showMessageRequestParams.setMessage(
+                        "Language Server has stopped working. Compilation has failed due to corrupted modules.");
+                showMessageRequestParams.setActions(List.of(new MessageActionItem(PULL_MODULES_ACTION)));
+
+                client.showMessageRequest(showMessageRequestParams).thenAccept(action -> {
+                    if (action != null && PULL_MODULES_ACTION.equals(action.getTitle())) {
+                        PullModuleExecutor.resolveModules(context.fileUri(), client, context.workspace(),
+                                context.languageServercontext());
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+}
