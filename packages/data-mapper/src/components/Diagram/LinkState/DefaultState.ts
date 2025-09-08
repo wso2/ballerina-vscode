1077c9b27e81d88a11666bc1ef3875dd79f70793
madushajg <madushajg@gmail.com>
2025-09-08 15:08:55 +0530

    Fix seleting sub-mapping input port by clicking on the field

--- packages/data-mapper/src/components/Diagram/LinkState/DefaultState.ts
+++ packages/data-mapper/src/components/Diagram/LinkState/DefaultState.ts
@@ -28,7 +28,7 @@ import {
 import { DiagramEngine, DragDiagramItemsState, PortModel } from '@projectstorm/react-diagrams-core';
 
 import { DMCanvasContainerID } from "../Canvas/DataMapperCanvasWidget";
-import { ArrayOutputNode, InputNode, ObjectOutputNode, QueryOutputNode } from '../Node';
+import { ArrayOutputNode, InputNode, ObjectOutputNode, QueryOutputNode, SubMappingNode } from '../Node';
 import { DataMapperNodeModel } from "../Node/commons/DataMapperNode";
 import { LinkOverayContainerID } from '../OverriddenLinkLayer/LinkOverlayPortal';
 import { CreateLinkState } from './CreateLinkState';
@@ -96,6 +96,7 @@ export class DefaultState extends State<DiagramEngine> {
 							|| element instanceof ArrayOutputNode
 							|| element instanceof QueryOutputNode
 							|| element instanceof InputNode
+							|| element instanceof SubMappingNode
 						)
 					) {
 						this.transitionWithEvent(this.createLink, actionEvent);
