7f830149f29b1e45681589f63f128916bf8ba86d
Senith Uthsara <senithkarunarathneu@gmail.com>
2025-09-09 15:13:52 +0530

    fix variable trimming

--- packages/ballerina-visualizer/src/views/BI/HelperPaneNew/Views/Variables.tsx
+++ packages/ballerina-visualizer/src/views/BI/HelperPaneNew/Views/Variables.tsx
@@ -81,7 +81,7 @@ const VariableItem = ({ item, onItemSelect, onMoreIconClick }: VariableItemProps
                 <Typography 
                     variant="body3"
                     sx={{
-                        maxWidth: isHovered ? 'none' : '20ch',
+                        maxWidth: isHovered ? '30ch' : '20ch',
                         overflow: 'hidden',
                         textOverflow: 'ellipsis',
                         whiteSpace: isHovered ? 'normal' : 'nowrap',
