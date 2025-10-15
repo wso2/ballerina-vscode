8fd97c10060d66ad8a6dabb262c6cac14e363a73
Senith Uthsara <senithkarunarathneu@gmail.com>
2025-10-15 10:50:15 +0530

    fix ui changes

--- packages/ballerina-side-panel/src/components/editors/ExpressionField.tsx
+++ packages/ballerina-side-panel/src/components/editors/ExpressionField.tsx
@@ -22,6 +22,7 @@ import {
     FormExpressionEditor,
     FormExpressionEditorRef,
     HelperPaneHeight,
+    ThemeColors,
     Tooltip
 } from '@wso2/ui-toolkit';
 import { InputMode } from './ChipExpressionEditor/types';
@@ -69,7 +70,8 @@ const EditorRibbon = ({ onClick }: { onClick: () => void }) => {
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
-                    viewBox="0 0 24 24">
+                    viewBox="0 0 24 24"
+                    style={{ color: ThemeColors.ON_PRIMARY }}>
                     <path
                         fill="currentColor"
                         d="M12.42 5.29c-1.1-.1-2.07.71-2.17 1.82L10 10h2.82v2h-3l-.44 5.07A4.001 4.001 0 0 1 2 18.83l1.5-1.5c.33 1.05 1.46 1.64 2.5 1.3c.78-.24 1.33-.93 1.4-1.74L7.82 12h-3v-2H8l.27-3.07a4.01 4.01 0 0 1 4.33-3.65c1.26.11 2.4.81 3.06 1.89l-1.5 1.5c-.25-.77-.93-1.31-1.74-1.38M22 13.65l-1.41-1.41l-2.83 2.83l-2.83-2.83l-1.43 1.41l2.85 2.85l-2.85 2.81l1.43 1.41l2.83-2.83l2.83 2.83L22 19.31l-2.83-2.81z" />
