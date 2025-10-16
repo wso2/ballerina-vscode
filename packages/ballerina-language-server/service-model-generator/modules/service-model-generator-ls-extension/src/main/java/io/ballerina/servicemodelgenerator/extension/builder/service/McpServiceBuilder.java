e3d18712b8ca97ac9b63bc7b1fb2afdae8c44548
Azeem Muzammil <azeemmuza@gmail.com>
2025-10-16 11:54:12 +0530

    Fix double quote issue

--- packages/ballerina-language-server/service-model-generator/modules/service-model-generator-ls-extension/src/main/java/io/ballerina/servicemodelgenerator/extension/builder/service/McpServiceBuilder.java
+++ packages/ballerina-language-server/service-model-generator/modules/service-model-generator-ls-extension/src/main/java/io/ballerina/servicemodelgenerator/extension/builder/service/McpServiceBuilder.java
@@ -88,8 +88,8 @@ public class McpServiceBuilder extends AbstractServiceBuilder {
     private String buildServiceConfig(String name, String version) {
         return "@" + MCP + ":ServiceConfig" + SPACE + OPEN_BRACE + NEW_LINE +
                TAB + "info: " + OPEN_BRACE + NEW_LINE +
-               TWO_TABS + "name: \"" + name + "\"," + NEW_LINE +
-               TWO_TABS + "version: \"" + version + "\"" + NEW_LINE +
+               TWO_TABS + "name: " + name + NEW_LINE +
+               TWO_TABS + "version: " + version + NEW_LINE +
                TAB + CLOSE_BRACE + NEW_LINE +
                CLOSE_BRACE + NEW_LINE;
     }
