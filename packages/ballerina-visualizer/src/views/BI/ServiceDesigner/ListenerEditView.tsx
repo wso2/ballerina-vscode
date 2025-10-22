ebad1b4ed8d3338edd848ef123ee121647db5630
Anjana S Porawagama <axewilledge123@gmail.com>
2025-10-22 18:13:41 +0530

    Refactor Service and Listener Configuration Views

--- packages/ballerina-visualizer/src/views/BI/ServiceDesigner/ListenerEditView.tsx
+++ packages/ballerina-visualizer/src/views/BI/ServiceDesigner/ListenerEditView.tsx
@@ -108,8 +108,8 @@ export function ListenerEditView(props: ListenerEditViewProps) {
 
     return (
         <View>
-            {/* <TopNavigationBar />
-            <TitleBar title="Listener" subtitle="Configure Listener" /> */}
+            <TopNavigationBar />
+            <TitleBar title={listenerModel?.name + " Configuration"} subtitle="Configure Listener" />
             <ViewContent padding>
                 {!listenerModel &&
                     <LoadingContainer>
