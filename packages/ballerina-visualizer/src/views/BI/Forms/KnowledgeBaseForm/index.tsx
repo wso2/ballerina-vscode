82dacfa091ad3e4c9954d5e689d4c1a09d9440c9
Dan Niles <niles_dan@live.com>
2025-10-23 00:05:03 +0530

    Enhance KnowledgeBaseForm with project path handling and update node property value logic

--- packages/ballerina-visualizer/src/views/BI/Forms/KnowledgeBaseForm/index.tsx
+++ packages/ballerina-visualizer/src/views/BI/Forms/KnowledgeBaseForm/index.tsx
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Codicon } from "@wso2/ui-toolkit";
 import styled from "@emotion/styled";
 
@@ -40,6 +40,7 @@ import {
 } from "../form-utils";
 import { SidePanelView } from "../../FlowDiagram/PanelManager";
 import { ConnectionKind } from "../../../../components/ConnectionSelector";
+import { URI, Utils } from "vscode-uri";
 
 const DEFAULT_CHUNKER_VALUE = "\"AUTO\"";
 
@@ -113,6 +114,14 @@ export function KnowledgeBaseForm(props: KnowledgeBaseFormProps) {
     const [knowledgeBaseFormValues, setKnowledgeBaseFormValues] = useState<FormValues>({});
     const [saving, setSaving] = useState<boolean>(false);
 
+    const CONNECTIONS_FILE = "connections.bal";
+    const projectPath = useRef<string>("");
+    const targetLineRangeRef = useRef<LineRange>({
+        startLine: { line: 0, offset: 0 },
+        endLine: { line: 0, offset: 0 }
+    });
+    const connectionsFilePath = useRef<string>("");
+
     useEffect(() => {
         initializeForm();
         handleFormOpen();
@@ -152,7 +161,7 @@ export function KnowledgeBaseForm(props: KnowledgeBaseFormProps) {
         }
     };
 
-    const initializeForm = () => {
+    const initializeForm = async () => {
         const formProperties = getFormProperties(node);
         const fields = convertNodePropertiesToFormFields(formProperties);
         const actionFields = ["vectorStore", "embeddingModel", "chunker"];
@@ -175,6 +184,28 @@ export function KnowledgeBaseForm(props: KnowledgeBaseFormProps) {
         });
         setKnowledgeBaseFields(fields);
         setFormImports(getImportsForFormFields(fields));
+
+        projectPath.current = await rpcClient.getVisualizerLocation().then((location) => location.projectUri);
+        connectionsFilePath.current = Utils.joinPath(URI.file(projectPath.current), CONNECTIONS_FILE).fsPath;
+        const endPosition = await rpcClient.getBIDiagramRpcClient().getEndOfFile({
+            filePath: connectionsFilePath.current
+        });
+        targetLineRangeRef.current = {
+            startLine: {
+                line: endPosition.line,
+                offset: endPosition.offset
+            },
+            endLine: {
+                line: endPosition.line,
+                offset: endPosition.offset
+            }
+        }
+    };
+
+    const updateNodePropertyValue = (fieldKey: string, value: any): void => {
+        if (node.properties && node.properties[fieldKey as keyof typeof node.properties]) {
+            node.properties[fieldKey as keyof typeof node.properties].value = value;
+        }
     };
 
     const mergeFormDataWithFlowNode = (data: FormValues, targetLineRange: LineRange): FlowNode => {
@@ -205,8 +236,8 @@ export function KnowledgeBaseForm(props: KnowledgeBaseFormProps) {
                         <S.FormWrapper>
                             <Form
                                 formFields={knowledgeBaseFields}
-                                fileName={fileName}
-                                targetLineRange={targetLineRange}
+                                fileName={connectionsFilePath.current}
+                                targetLineRange={targetLineRangeRef.current}
                                 selectedNode={node.codedata.node}
                                 expressionEditor={expressionEditor}
                                 openSubPanel={openSubPanel}
@@ -219,6 +250,7 @@ export function KnowledgeBaseForm(props: KnowledgeBaseFormProps) {
                                 disableSaveButton={!isFormValid}
                                 isSaving={showProgressIndicator || saving}
                                 onChange={(fieldKey, value, allValues) => {
+                                    updateNodePropertyValue(fieldKey, value);
                                     const isValid = allValues["vectorStore"] !== "" && allValues["embeddingModel"] !== "";
                                     setIsFormValid(isValid);
                                     setKnowledgeBaseFormValues(allValues);
