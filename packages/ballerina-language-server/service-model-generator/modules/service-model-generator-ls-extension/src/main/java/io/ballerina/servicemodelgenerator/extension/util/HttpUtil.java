b1196bca72b028d7d07fc26cb2d0f266ebd40832
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2025-10-30 18:32:30 +0530

    Fix issue with http param annots

--- packages/ballerina-language-server/service-model-generator/modules/service-model-generator-ls-extension/src/main/java/io/ballerina/servicemodelgenerator/extension/util/HttpUtil.java
+++ packages/ballerina-language-server/service-model-generator/modules/service-model-generator-ls-extension/src/main/java/io/ballerina/servicemodelgenerator/extension/util/HttpUtil.java
@@ -575,9 +575,9 @@ public final class HttpUtil {
                 } else {
                     Value queryAnnot = properties.get("annotQuery");
                     Value payloadAnnot = properties.get("annotPayload");
-                    if (Objects.nonNull(queryAnnot)) {
+                    if (Objects.nonNull(queryAnnot) && Objects.nonNull(queryAnnot.getValue())) {
                         paramDef.append("@http:").append(HTTP_QUERY_PARAM_ANNOTATION).append(queryAnnot.getValue());
-                    } else if (Objects.nonNull(payloadAnnot)) {
+                    } else if (Objects.nonNull(payloadAnnot) && Objects.nonNull(payloadAnnot.getValue())) {
                         paramDef.append("@http:").append(HTTP_PAYLOAD_PARAM_ANNOTATION).append(payloadAnnot.getValue());
                     } else if (httpParamType.equals(HTTP_PARAM_TYPE_QUERY)) {
                         paramDef.append("@http:").append(HTTP_QUERY_PARAM_ANNOTATION);
