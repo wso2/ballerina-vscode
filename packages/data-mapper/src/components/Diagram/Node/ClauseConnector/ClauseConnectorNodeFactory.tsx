316f4f395569c4f84b74dc4cdc4a22f901fa3c4e
ChamodA <chamoda@wso2.com>
2025-11-02 19:43:53 +0530

    Add ClauseConnectorNode, factory, widget, and index files

---
+++ packages/data-mapper/src/components/Diagram/Node/ClauseConnector/ClauseConnectorNodeFactory.tsx
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import * as React from 'react';
+
+import { AbstractReactFactory } from '@projectstorm/react-canvas-core';
+import { DiagramEngine } from '@projectstorm/react-diagrams-core';
+
+import { ClauseConnectorNode, CLAUSE_CONNECTOR_NODE_TYPE } from './ClauseConnectorNode';
+import { ClauseConnectorNodeWidget } from './ClauseConnectorNodeWidget';
+
+export class ClauseConnectorNodeFactory extends AbstractReactFactory<ClauseConnectorNode, DiagramEngine> {
+	constructor() {
+		super(CLAUSE_CONNECTOR_NODE_TYPE);
+	}
+
+	generateReactWidget(event: { model: ClauseConnectorNode; }): JSX.Element {
+		const inputPortHasLinks = Object.keys(event.model.inPort.links).length;
+		const outputPortHasLinks = Object.keys(event.model.outPort.links).length;
+		if (inputPortHasLinks && outputPortHasLinks) {
+			return <ClauseConnectorNodeWidget engine={this.engine} node={event.model} />;
+		}
+		return null;
+	}
+
+	generateModel(): ClauseConnectorNode {
+		return undefined;
+	}
+}
