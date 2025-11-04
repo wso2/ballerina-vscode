08ecbe1d904812e7166e66335d5ebefbb73b895c
ChamodA <chamoda@wso2.com>
2025-11-04 17:23:46 +0530

    Remove variable and source code in datamapper codedata for re-usable data mapper

--- packages/ballerina-extension/src/rpc-managers/data-mapper/utils.ts
+++ packages/ballerina-extension/src/rpc-managers/data-mapper/utils.ts
@@ -33,7 +33,8 @@ import {
     IOTypeField,
     IORoot,
     ExpandModelOptions,
-    ExpandedDMModel
+    ExpandedDMModel,
+    MACHINE_VIEW
 } from "@wso2/ballerina-core";
 import { updateSourceCode, UpdateSourceCodeRequest } from "../../utils";
 import { StateMachine, updateDataMapperView } from "../../stateMachine";
@@ -80,6 +81,11 @@ export async function fetchDataMapperCodeData(
     const response = await StateMachine
         .langClient()
         .getDataMapperCodedata({ filePath, codedata: modifiedCodeData, name: varName });
+
+    if (StateMachine.context().view === MACHINE_VIEW.DataMapper) {
+        const { node, ...cleanCodeData } = response.codedata;
+        return cleanCodeData;
+    }
     return response.codedata;
 }
 
@@ -368,7 +374,9 @@ function updateView(codeData: CodeData | null, varName: string): void {
         return;
     }
 
-    applySourceCodeHack(codeData);
+    if (StateMachine.context().view === MACHINE_VIEW.InlineDataMapper) {
+        applySourceCodeHack(codeData);
+    }
     updateDataMapperView(codeData, varName);
 }
 
