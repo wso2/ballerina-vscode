7e0b4be6767cc50501b3a23bdb7bc9959f50935f
kaje94 <a.kajendran@gmail.com>
2025-11-07 16:57:18 +0530

    refactor platform ext context into new file

--- packages/ballerina-visualizer/src/Context.tsx
+++ packages/ballerina-visualizer/src/Context.tsx
@@ -16,12 +16,10 @@
  * under the License.
  */
 
-import React, { ReactNode, useRef, useState, createContext, useContext, FC, useEffect } from "react";
-import { BallerinaRpcClient, VisualizerContext as RpcContext, Context, useRpcContext } from "@wso2/ballerina-rpc-client";
+import React, { ReactNode, useRef, useState, createContext, useContext } from "react";
+import { BallerinaRpcClient, VisualizerContext as RpcContext, Context } from "@wso2/ballerina-rpc-client";
 import { NodePosition, STNode } from "@wso2/syntax-tree";
 import { ConnectorInfo, TriggerModelsResponse} from "@wso2/ballerina-core";
-import { PlatformExtState } from "@wso2/ballerina-core/lib/rpc-types/platform-ext/interfaces";
-import { useQueryClient, useQuery } from "@tanstack/react-query";
 
 export function RpcContextProvider({ children }: { children: ReactNode }) {
     const rpcClient = useRef(new BallerinaRpcClient());
@@ -185,44 +183,3 @@ export const ModalStackProvider = ({children}: {children: ReactNode}) => {
 }
 
 export const useModalStack = () => useContext(ModalStackContext);
-
-// Platform extension context
-const defaultPlatformExtContext: { state: PlatformExtState | null } = { state: {components: [], isLoggedIn: false }};
-
-const PlatformExtContext = React.createContext(defaultPlatformExtContext);
-
-export const usePlatformExtContext = () => {
-	return useContext(PlatformExtContext) || defaultPlatformExtContext;
-};
-
-export const PlatformExtContextProvider: FC<{children: ReactNode}> = ({ children }) => {
-	const queryClient = useQueryClient();
-    const { rpcClient } = useRpcContext();
-
-	const {
-		data: platformExtState,
-	} = useQuery({
-		queryKey: ["platform-ext-state"],
-		queryFn: async () => {
-            const projectPath = await rpcClient.getVisualizerLocation()
-            const isLoggedIn = await rpcClient.getPlatformRpcClient().isLoggedIn();
-            const components = await rpcClient.getPlatformRpcClient().getDirectoryComponents(projectPath.projectUri)
-            const selectedContext = await rpcClient.getPlatformRpcClient().getSelectedContext();
-            // todo: get state as it is instead of individual functions
-            return { isLoggedIn, components, selectedContext } as PlatformExtState
-        },
-		refetchOnWindowFocus: true,
-	});
-
-	useEffect(() => {
-		rpcClient.getPlatformRpcClient().onPlatformExtStoreStateChange((state) => {
-			queryClient.setQueryData(["platform-ext-state"], state);
-		});
-	}, []);
-
-	return (
-		<PlatformExtContext.Provider value={{ state: platformExtState}}>
-			{children}
-		</PlatformExtContext.Provider>
-	);
-};
