d47ec5c1fe1a6f3a17b3b380380bae9eeb1ced6a
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2025-11-09 00:17:50 +0530

    Update tests

--- packages/ballerina-language-server/architecture-model-generator/modules/architecture-model-generator-ls-extension/src/test/java/io/ballerina/designmodelgenerator/extension/ArtifactsTest.java
+++ packages/ballerina-language-server/architecture-model-generator/modules/architecture-model-generator-ls-extension/src/test/java/io/ballerina/designmodelgenerator/extension/ArtifactsTest.java
@@ -22,6 +22,8 @@ import com.google.gson.JsonObject;
 import io.ballerina.designmodelgenerator.extension.request.ArtifactsRequest;
 import io.ballerina.modelgenerator.commons.AbstractLSTest;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -83,5 +85,15 @@ public class ArtifactsTest extends AbstractLSTest {
     }
 
     public record TestConfig(String description, String source, JsonObject output) {
+    }
+
+    @AfterMethod
+    public void shutDownLanguageServer() {
+        super.shutDownLanguageServer();
+    }
+
+    @BeforeMethod
+    public void startLanguageServer() {
+        super.startLanguageServer();
     }
 }
