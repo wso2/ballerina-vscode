13f9d70d4e089efd068e5eb0916c4c338f3f8169
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2025-11-10 19:24:36 +0530

    Rollback to previous project loading impl

--- packages/ballerina-language-server/langserver-core/src/main/java/org/ballerinalang/langserver/version/BallerinaBaseCompilerApi.java
+++ packages/ballerina-language-server/langserver-core/src/main/java/org/ballerinalang/langserver/version/BallerinaBaseCompilerApi.java
@@ -27,12 +27,22 @@ import io.ballerina.projects.DiagnosticResult;
 import io.ballerina.projects.Document;
 import io.ballerina.projects.Project;
 import io.ballerina.projects.ProjectEnvironmentBuilder;
+import io.ballerina.projects.ProjectException;
+import io.ballerina.projects.ProjectKind;
+import io.ballerina.projects.directory.BuildProject;
 import io.ballerina.projects.directory.ProjectLoader;
+import io.ballerina.projects.directory.SingleFileProject;
+import io.ballerina.projects.util.ProjectConstants;
+import io.ballerina.projects.util.ProjectPaths;
 import io.ballerina.tools.diagnostics.Diagnostic;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.ballerinalang.annotation.JavaSPIService;
+import org.ballerinalang.langserver.common.utils.CommonUtil;
 import org.ballerinalang.langserver.commons.BallerinaCompilerApi;
 import org.wso2.ballerinalang.compiler.tree.BLangPackage;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
@@ -109,8 +119,7 @@ public class BallerinaBaseCompilerApi extends BallerinaCompilerApi {
 
     @Override
     public Project loadProject(Path path, BuildOptions buildOptions) {
-        ProjectEnvironmentBuilder envBuilder = ProjectEnvironmentBuilder.getDefaultBuilder();
-        return ProjectLoader.loadProject(path, envBuilder);
+        return createProject(path, buildOptions);
     }
 
     @Override
@@ -132,4 +141,49 @@ public class BallerinaBaseCompilerApi extends BallerinaCompilerApi {
     public List<Project> getWorkspaceProjects(Project project) {
         return Collections.emptyList();
     }
+
+    private Project createProject(Path filePath, BuildOptions buildOptions) {
+        Pair<ProjectKind, Path> projectKindAndProjectRootPair = computeProjectKindAndProjectRoot(filePath);
+        ProjectKind projectKind = projectKindAndProjectRootPair.getLeft();
+        Path projectRoot = projectKindAndProjectRootPair.getRight();
+        try {
+            Project project;
+            if (projectKind == ProjectKind.BUILD_PROJECT) {
+                project = BuildProject.load(projectRoot, buildOptions);
+
+                // TODO: Remove this once https://github.com/ballerina-platform/ballerina-lang/issues/43972 is resolved
+                // Save the dependencies.toml to resolve the inconsistencies issue in the subsequent builds
+                if (BallerinaCompilerApi.getInstance().hasOptimizedDependencyCompilation(project)) {
+                    BuildOptions newOptions = BuildOptions.builder()
+                            .setOffline(CommonUtil.COMPILE_OFFLINE)
+                            .setSticky(false)
+                            .build();
+                    project = BuildProject.load(projectRoot, newOptions);
+                }
+            } else if (projectKind == ProjectKind.SINGLE_FILE_PROJECT) {
+                project = SingleFileProject.load(projectRoot, buildOptions);
+            } else {
+                // Projects other than single file and build will use the ProjectLoader.
+                project = ProjectLoader.loadProject(projectRoot, buildOptions);
+            }
+            return project;
+        } catch (ProjectException e) {
+            //If there is an error the project crash status should be set.
+            throw new ProjectException("failed to create project " +
+                    filePath.toString(), e);
+        }
+    }
+
+    private Pair<ProjectKind, Path> computeProjectKindAndProjectRoot(Path path) {
+        if (ProjectPaths.isStandaloneBalFile(path)) {
+            return new ImmutablePair<>(ProjectKind.SINGLE_FILE_PROJECT, path);
+        }
+        // Following is a temp fix to distinguish Bala and Build projects
+        Path tomlPath = ProjectPaths.packageRoot(path).resolve(ProjectConstants.BALLERINA_TOML);
+        if (Files.exists(tomlPath)) {
+            return new ImmutablePair<>(ProjectKind.BUILD_PROJECT, ProjectPaths.packageRoot(path));
+        }
+        return new ImmutablePair<>(ProjectKind.BALA_PROJECT, ProjectPaths.packageRoot(path));
+    }
+
 }
