d13af87b15bb4241f29b24eb8107f82111b509d1
ChamodA <chamoda@wso2.com>
2025-11-22 20:19:46 +0530

    Update mapSeqToPrimitive to adjust letClauseIndex based on GROUP_BY clause index

--- packages/data-mapper/src/components/Diagram/utils/modification-utils.ts
+++ packages/data-mapper/src/components/Diagram/utils/modification-utils.ts
@@ -210,8 +210,13 @@ export async function mapSeqToPrimitive(link: DataMapperLinkModel, context: IDat
 				expression: sourcePortModel.attributes.fieldFQN,
 			}
 		}
-		
-		await context.addClauses(clause, viewId, true, -1);
+
+		const groupByClauseIndex = context.model.query?.intermediateClauses
+			?.findIndex(c => c.type === IntermediateClauseType.GROUP_BY);
+
+		const letClauseIndex = (groupByClauseIndex !== -1 && groupByClauseIndex !== undefined) ? groupByClauseIndex - 1 : -1;
+
+		await context.addClauses(clause, viewId, true, letClauseIndex);
 
 		sourcePortModel.attributes.fieldFQN = clause.properties.name;
 		sourcePortModel.attributes.optionalOmittedFieldFQN = clause.properties.name;
