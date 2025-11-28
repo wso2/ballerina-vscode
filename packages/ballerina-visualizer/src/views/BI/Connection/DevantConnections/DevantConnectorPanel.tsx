b1f23944e98b3d0235d35e215cdd55b912f4b869
kaje94 <a.kajendran@gmail.com>
2025-11-28 13:21:07 +0530

    fix openapi spec display for 3rd party apis

--- packages/ballerina-visualizer/src/views/BI/Connection/DevantConnections/DevantConnectorPanel.tsx
+++ packages/ballerina-visualizer/src/views/BI/Connection/DevantConnections/DevantConnectorPanel.tsx
@@ -30,7 +30,7 @@ export const DevantConnectorPanel: FC<{ selectedItem: MarketplaceItem; onClose: 
 }) => {
     const [showInfo, setShowInfo] = useState(false);
     const { platformExtState } = usePlatformExtContext();
-
+    
     return (
         <>
             <>
