3a65d60d91c66798d835e1ddd85db96483083871
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2025-12-08 15:59:49 +0530

    Add License headers

--- packages/ballerina-language-server/architecture-model-generator/modules/architecture-model-generator-core/src/main/java/io/ballerina/copilotagent/core/ServiceMethodExtractor.java
+++ packages/ballerina-language-server/architecture-model-generator/modules/architecture-model-generator-core/src/main/java/io/ballerina/copilotagent/core/ServiceMethodExtractor.java
@@ -1,8 +1,24 @@
+/*
+ *  Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com)
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package io.ballerina.copilotagent.core;
 
 import io.ballerina.compiler.syntax.tree.FunctionDefinitionNode;
-import io.ballerina.compiler.syntax.tree.FunctionSignatureNode;
-import io.ballerina.compiler.syntax.tree.MethodDeclarationNode;
 import io.ballerina.compiler.syntax.tree.Node;
 import io.ballerina.compiler.syntax.tree.NodeList;
 import io.ballerina.compiler.syntax.tree.NodeVisitor;
@@ -12,6 +28,11 @@ import io.ballerina.copilotagent.core.models.ServiceMemberMap;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+/**
+ * Visitor to extract service methods and populate the ServiceMemberMap.
+ *
+ * @since 1.5.0
+ */
 public class ServiceMethodExtractor extends NodeVisitor {
 
     private final ServiceMemberMap serviceMemberMap;
--- packages/ballerina-language-server/architecture-model-generator/modules/architecture-model-generator-ls-extension/src/test/java/io/ballerina/copilotagent/extension/SemanticDiffComputerTest.java
+++ packages/ballerina-language-server/architecture-model-generator/modules/architecture-model-generator-ls-extension/src/test/java/io/ballerina/copilotagent/extension/SemanticDiffComputerTest.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com)
+ *  Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com)
  *
  *  WSO2 LLC. licenses this file to you under the Apache License,
  *  Version 2.0 (the "License"); you may not use this file except
@@ -43,9 +43,9 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
- * Tests for getting the design model for a package.
+ * Test for comparing file schema projects and ai schema projects.
  *
- * @since 1.0.0
+ * @since 1.5.0
  */
 public class SemanticDiffComputerTest extends AbstractLSTest {
 
