f556bbb94b0765d67124b3fbe7784071840c85d6
madushajg <madushajg@gmail.com>
2025-12-09 16:11:25 +0530

    Add command utilities for workspace handling and package selection

---
+++ packages/ballerina-extension/src/utils/command-utils.ts
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { window } from "vscode";
+import { MACHINE_VIEW } from "@wso2/ballerina-core";
+
+export function isAtWorkspaceLevel(
+    workspacePath: string | undefined,
+    view: MACHINE_VIEW | undefined,
+    projectPath: string | undefined,
+    isWebviewOpen: boolean,
+    hasActiveTextEditor: boolean
+): boolean {
+    return !!(
+        workspacePath &&
+        (view === MACHINE_VIEW.WorkspaceOverview || !projectPath || !isWebviewOpen) &&
+        !hasActiveTextEditor
+    );
+}
+
+export async function promptPackageSelection(
+    availablePackages: string[],
+    placeHolder?: string
+): Promise<string | undefined> {
+    return window.showQuickPick(availablePackages, {
+        placeHolder: placeHolder || "Select a package",
+        ignoreFocusOut: false
+    });
+}
+
+export function needsProjectDiscovery(
+    projectInfo: any,
+    projectRoot: string | undefined,
+    projectPath: string | undefined
+): boolean {
+    return !projectInfo || (!!projectRoot && projectPath !== projectRoot);
+}
