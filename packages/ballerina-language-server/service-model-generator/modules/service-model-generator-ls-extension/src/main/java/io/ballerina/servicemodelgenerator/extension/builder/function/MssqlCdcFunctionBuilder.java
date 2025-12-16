c717528bbf0c74475c1f61ed044a7682bd5a6fcb
Radith Samarakoon <jjeli1234@gmail.com>
2025-12-17 01:01:53 +0530

    Implement dual databinding parameter for onUpdate
      Add special handling for the onUpdate function to show a single
      databinding parameter in the UI that expands to two parameters
      (beforeEntry and afterEntry) during code generation.
    
      Changes:
      - MssqlCdcServiceBuilder: Add combineDatabindingParams() to merge
        beforeEntry and afterEntry into single UI parameter after
        consolidation and databinding processing
      - MssqlCdcServiceBuilder: Add setModuleName() utility to ensure
        function codedata exists and set module name for router
      - MssqlCdcFunctionBuilder: Implement addModel() and updateModel()
        to expand single parameter back to two during code generation
      - MssqlCdcFunctionBuilder: Add expandDatabindingParams() to copy
        type from afterEntry to beforeEntry before generating code
    
      Rationale: onUpdate is triggered when a database row is updated, so
      beforeEntry and afterEntry must have the same type. Showing two
      separate parameters in the UI is redundant and error-prone. This
      change simplifies the UX by presenting one parameter that generates
      both in the code.
    
      The module name setting ensures the router correctly invokes
      MssqlCdcFunctionBuilder for both functions in source and functions
      added from the UI

--- packages/ballerina-language-server/service-model-generator/modules/service-model-generator-ls-extension/src/main/java/io/ballerina/servicemodelgenerator/extension/builder/function/MssqlCdcFunctionBuilder.java
+++ packages/ballerina-language-server/service-model-generator/modules/service-model-generator-ls-extension/src/main/java/io/ballerina/servicemodelgenerator/extension/builder/function/MssqlCdcFunctionBuilder.java
@@ -18,57 +18,86 @@
 
 package io.ballerina.servicemodelgenerator.extension.builder.function;
 
+import io.ballerina.servicemodelgenerator.extension.model.Function;
+import io.ballerina.servicemodelgenerator.extension.model.Parameter;
 import io.ballerina.servicemodelgenerator.extension.model.context.AddModelContext;
 import io.ballerina.servicemodelgenerator.extension.model.context.UpdateModelContext;
-import io.ballerina.servicemodelgenerator.extension.util.DatabindUtil;
 import org.eclipse.lsp4j.TextEdit;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static io.ballerina.servicemodelgenerator.extension.builder.service.MssqlCdcServiceBuilder.AFTER_ENTRY_FIELD;
-import static io.ballerina.servicemodelgenerator.extension.builder.service.MssqlCdcServiceBuilder.TYPE_PREFIX;
+import static io.ballerina.servicemodelgenerator.extension.builder.service.MssqlCdcServiceBuilder.BEFORE_ENTRY_FIELD;
+import static io.ballerina.servicemodelgenerator.extension.util.Constants.DATA_BINDING;
 import static io.ballerina.servicemodelgenerator.extension.util.Constants.MSSQL;
 
 /**
- * Represents the MSSQL CDC function builder of the service model generator.
- * Handles data binding for CDC event parameters (afterEntry and beforeEntry).
+ * Represents the MSSQL CDC function builder of the service model generator. Handles special-case logic for onUpdate
+ * function which has two databinding parameters (beforeEntry and afterEntry) that must share the same type.
  *
  * @since 1.5.0
  */
 public final class MssqlCdcFunctionBuilder extends AbstractFunctionBuilder {
 
-    private static final String REQUIRED_PARAM_TYPE = "record {}";
-//
-//    @Override
-//    public Map<String, List<TextEdit>> updateModel(UpdateModelContext context) {
-//        // Process databinding - handles type generation/update and parameter updates
-//        // Note: CDC uses record {} as the base type, not array type (isArray = false)
-//        Map<String, List<TextEdit>> databindEdits = DatabindUtil.processDatabindingUpdate(
-//                context, TYPE_PREFIX, REQUIRED_PARAM_TYPE, AFTER_ENTRY_FIELD, false);
-//
-//        // Get edits for main file
-//        Map<String, List<TextEdit>> mainFileEdits = super.updateModel(context);
-//
-//        // Merge both edits into a mutable map
-//        Map<String, List<TextEdit>> allEdits = new HashMap<>(mainFileEdits);
-//        allEdits.putAll(databindEdits);
-//
-//        return allEdits;
-//    }
-//
-//    @Override
-//    public Map<String, List<TextEdit>> addModel(AddModelContext context) throws Exception {
-//        // Process databinding when adding new functions
-//        Map<String, List<TextEdit>> databindEdits = DatabindUtil.processDatabindingForAdd(
-//                context, TYPE_PREFIX, REQUIRED_PARAM_TYPE, AFTER_ENTRY_FIELD, false);
-//
-//        Map<String, List<TextEdit>> mainFileEdits = super.addModel(context);
-//        Map<String, List<TextEdit>> allEdits = new HashMap<>(mainFileEdits);
-//        allEdits.putAll(databindEdits);
-//        return allEdits;
-//    }
+    private static final String ON_UPDATE_FUNCTION = "onUpdate";
+
+    @Override
+    public Map<String, List<TextEdit>> addModel(AddModelContext context) throws Exception {
+        Function function = context.function();
+        if (ON_UPDATE_FUNCTION.equals(function.getName().getValue())) {
+            expandDatabindingParams(function);
+        }
+
+        // Call parent which will generate code with expanded parameters
+        return super.addModel(context);
+    }
+
+    @Override
+    public Map<String, List<TextEdit>> updateModel(UpdateModelContext context) {
+        Function function = context.function();
+        if (ON_UPDATE_FUNCTION.equals(function.getName().getValue())) {
+            expandDatabindingParams(function);
+        }
+
+        // Call parent which will generate code with expanded parameters
+        return super.updateModel(context);
+    }
+
+    /**
+     * Expands the single databinding parameter (afterEntry) to two parameters (beforeEntry and afterEntry) for the
+     * onUpdate function. This is called before code generation to ensure both parameters are present in the generated
+     * signature with the same type.
+     * <p>
+     * This method looks for the case where afterEntry is enabled (visible in UI) and beforeEntry is disabled (hidden
+     * from UI). It then enables beforeEntry and copies the type from afterEntry.
+     * </p>
+     *
+     * @param function The onUpdate function to process
+     */
+    private void expandDatabindingParams(Function function) {
+        List<Parameter> parameters = function.getParameters();
+        Parameter beforeEntry = null;
+        Parameter afterEntry = null;
+
+        for (Parameter param : parameters) {
+            if (!DATA_BINDING.equals(param.getKind())) {
+                continue;
+            }
+            String paramName = param.getName().getValue();
+            if (BEFORE_ENTRY_FIELD.equals(paramName)) {
+                beforeEntry = param;
+            } else if (AFTER_ENTRY_FIELD.equals(paramName)) {
+                afterEntry = param;
+            }
+        }
+
+        if (beforeEntry != null && afterEntry != null && !beforeEntry.isEnabled() && afterEntry.isEnabled()) {
+            beforeEntry.setEnabled(true);
+            beforeEntry.getType().setValue(afterEntry.getType().getValue());
+        }
+
+    }
 
     @Override
     public String kind() {
