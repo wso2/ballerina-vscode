baa5fb13d60ed31872d088faa7232629ec3d6ff3
gayaldassanayake <g.c.dassanayake@gmail.com>
2025-12-18 05:05:14 +0530

    Fix icon issue

--- packages/ballerina-visualizer/src/views/BI/ComponentListView/EventIntegrationPanel.tsx
+++ packages/ballerina-visualizer/src/views/BI/ComponentListView/EventIntegrationPanel.tsx
@@ -117,8 +117,8 @@ export function getCustomEntryNodeIcon(type: string) {
             return <Icon name="bi-mcp" />;
         case "solace":
             return <Icon name="bi-solace" sx={{ color: "#00C895" }}/>;
-        case "cdc-mssql":
-            return <Icon name="bi-mssql"/>;
+        case "mssql":
+            return <Icon name="bi-mssql" sx={{ color: "#b61d1c" }}/>;
         default:
             return null;
     }
