8f9e860d9e9c23a6645f2f91d1aeb5df35525640
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2025-12-17 21:49:06 +0530

    Address review suggestions

--- packages/ballerina-language-server/architecture-model-generator/modules/architecture-model-generator-ls-extension/src/main/java/io/ballerina/copilotagent/extension/response/AbstractResponse.java
+++ packages/ballerina-language-server/architecture-model-generator/modules/architecture-model-generator-ls-extension/src/main/java/io/ballerina/copilotagent/extension/response/AbstractResponse.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com)
+ *  Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com)
  *
  *  WSO2 LLC. licenses this file to you under the Apache License,
  *  Version 2.0 (the "License"); you may not use this file except
@@ -21,9 +21,9 @@ package io.ballerina.copilotagent.extension.response;
 import java.util.Arrays;
 
 /**
- * Represents the response for the design model getDesignModel API.
+ * Represents the response for the copilotAgentService API.
  *
- * @since 1.0.0
+ * @since 1.5.0
  */
 public class AbstractResponse {
 
