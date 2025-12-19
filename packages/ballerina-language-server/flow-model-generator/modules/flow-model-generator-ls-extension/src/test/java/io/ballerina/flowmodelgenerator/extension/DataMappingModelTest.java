f63166fc622c44ce59dfa33e34a172b3ac33ef8b
pasindufernando1 <sulakshana1234567@gmail.com>
2025-12-19 13:24:49 +0530

    Handle typenames for nil based primitive unions

--- packages/ballerina-language-server/flow-model-generator/modules/flow-model-generator-ls-extension/src/test/java/io/ballerina/flowmodelgenerator/extension/DataMappingModelTest.java
+++ packages/ballerina-language-server/flow-model-generator/modules/flow-model-generator-ls-extension/src/test/java/io/ballerina/flowmodelgenerator/extension/DataMappingModelTest.java
@@ -162,7 +162,8 @@ public class DataMappingModelTest extends AbstractLSTest {
                 {Path.of("query28.json")},
                 {Path.of("query29.json")},
                 {Path.of("variable58.json")},
-                {Path.of("variable59.json")}
+                {Path.of("variable59.json")},
+                {Path.of("variable60.json")}
         };
     }
 
