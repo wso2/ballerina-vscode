0f4b58686eb25903bfe6101c26c681d8136c387e
ChamodA <chamoda@wso2.com>
2026-01-06 16:02:20 +0530

    Fix ExpressionLabelWidget by removing trailing comma

--- packages/data-mapper/src/components/Diagram/Label/ExpressionLabelWidget.tsx
+++ packages/data-mapper/src/components/Diagram/Label/ExpressionLabelWidget.tsx
@@ -235,7 +235,7 @@ export function ExpressionLabelWidget(props: ExpressionLabelWidgetProps) {
         codeActions.push({
             title: "Map with query expression",
             onClick: onClickMapWithQuery
-        }, );
+        });
     } else if (mappingType === MappingType.ArrayToSingleton) {
         // TODO: Add impl
     }
