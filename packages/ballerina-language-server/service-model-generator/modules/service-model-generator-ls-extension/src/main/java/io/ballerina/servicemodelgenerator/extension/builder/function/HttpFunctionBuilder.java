ad5202bb45a89e8158b5a11dddd82358b1b0fa7d
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2026-01-09 11:26:42 +0530

    Add the import stmt to the top in types.bal

--- packages/ballerina-language-server/service-model-generator/modules/service-model-generator-ls-extension/src/main/java/io/ballerina/servicemodelgenerator/extension/builder/function/HttpFunctionBuilder.java
+++ packages/ballerina-language-server/service-model-generator/modules/service-model-generator-ls-extension/src/main/java/io/ballerina/servicemodelgenerator/extension/builder/function/HttpFunctionBuilder.java
@@ -381,7 +381,7 @@ public class HttpFunctionBuilder extends AbstractFunctionBuilder {
         });
         if (!typesBalImports.isEmpty()) {
             String importsStmts = String.join(NEW_LINE, typesBalImports);
-            typesBalTextEdits.add(new TextEdit(Utils.toRange(LinePosition.from(0, 0)), importsStmts));
+            typesBalTextEdits.addFirst(new TextEdit(Utils.toRange(LinePosition.from(0, 0)), importsStmts));
         }
     }
 
