326830a64e116b2f0abdd5bab4fa074ed368fdb1
samithkavishke <samithkarunathilake@gmail.com>
2026-02-02 13:55:27 +0530

    Fix the sqlite rebasing issue

--- packages/ballerina-language-server/model-generator-commons/src/main/java/io/ballerina/modelgenerator/commons/CommonUtils.java
+++ packages/ballerina-language-server/model-generator-commons/src/main/java/io/ballerina/modelgenerator/commons/CommonUtils.java
@@ -1371,6 +1371,20 @@ public class CommonUtils {
         return content.replace("\\\"", "\"").replace("\\\\", "\\");
     }
 
+    private static String escapeContent(String content) {
+        return content.replace("\\", "\\\\").replace("\"", "\\\"");
+    }
+
+    /**
+     * Unescapes backslashes and double quotes in the given content string.
+     *
+     * @param content the content string to unescape
+     * @return the unescaped content string
+     */
+    public static String unescapeContent(String content) {
+        return content.replace("\\\"", "\"").replace("\\\\", "\\");
+    }
+
     /**
      * Removes the leading single quote from the specified string if present.
      *
