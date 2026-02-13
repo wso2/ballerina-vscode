3c1e2a69816ef8ec66dfce56e43d35aef2c6c143
ChamodA <chamoda@wso2.com>
2026-02-14 00:10:16 +0530

    Enhance InputNode to support JSON and XML types, and add ConvertWidget component

--- packages/data-mapper/src/components/Diagram/Node/Input/InputNodeWidget.tsx
+++ packages/data-mapper/src/components/Diagram/Node/Input/InputNodeWidget.tsx
@@ -32,6 +32,7 @@ import { getTypeName } from "../../utils/type-utils";
 import { useShallow } from "zustand/react/shallow";
 import { InputCategoryIcon } from "./InputCategoryIcon";
 import { isGroupHeaderPort } from "../../utils/common-utils";
+import ConvertWidget from "../commons/ConvertWidget";
 
 export interface InputNodeWidgetProps {
     id: string; // this will be the root ID used to prepend for UUIDs of nested fields
@@ -132,56 +133,69 @@ export function InputNodeWidget(props: InputNodeWidgetProps) {
     };
 
     return (
-        <TreeContainer data-testid={`${id}-node`} onContextMenu={onRightClick}>
-            <TreeHeader
-                id={"recordfield-" + id}
-                isSelected={portState !== PortState.Unselected}
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                <span className={classes.label}>
-                    {fields && (
-                        <Button
-                            id={"expand-or-collapse-" + id} 
-                            appearance="icon"
-                            tooltip="Expand/Collapse"
-                            onClick={handleExpand}
-                            data-testid={`${id}-expand-icon-record-source-node`}
-                        >
-                            {expanded ? <Codicon name="chevron-down" /> : <Codicon name="chevron-right" />}
-                        </Button>
-                    )}
-                    {label}
-                    <InputCategoryIcon category={dmType.category} />
-                </span>
-                <span className={classes.outPort}>
-                    {portOut && (isNotGroupHeaderPort || !expanded && portOut.linkedPorts.length > 0) &&
-                        <DataMapperPortWidget engine={engine} port={portOut} handlePortState={handlePortState} />
-                    }
-                </span>
-            </TreeHeader>
-            {expanded && fields && (
-                <TreeBody>
-                    {
-                        fields
-                            ?.filter(f => !!f)
-                            .map((field, index) => {
-                            return (
-                                <InputNodeTreeItemWidget
-                                    key={index}
-                                    engine={engine}
-                                    dmType={field}
-                                    getPort={getPort}
-                                    parentId={id}
-                                    treeDepth={0}
-                                    hasHoveredParent={isHovered}
-                                    focusedInputs={focusedInputs}
-                                />
-                            );
-                        })
-                    }
-                </TreeBody>
-            )}
-        </TreeContainer>
+        <>
+            <TreeContainer data-testid={`${id}-node`} onContextMenu={onRightClick}>
+                <TreeHeader
+                    id={"recordfield-" + id}
+                    isSelected={portState !== PortState.Unselected}
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
+                >
+                    <span className={classes.label}>
+                        {(fields || dmType.convertedField) && (
+                            <Button
+                                id={"expand-or-collapse-" + id}
+                                appearance="icon"
+                                tooltip="Expand/Collapse"
+                                onClick={handleExpand}
+                                data-testid={`${id}-expand-icon-record-source-node`}
+                            >
+                                {expanded ? <Codicon name="chevron-down" /> : <Codicon name="chevron-right" />}
+                            </Button>
+                        )}
+                        {label}
+                        <InputCategoryIcon category={dmType.category} />
+                    </span>
+                    <span className={classes.outPort}>
+                        {portOut && (isNotGroupHeaderPort || !expanded && portOut.linkedPorts.length > 0) &&
+                            <DataMapperPortWidget engine={engine} port={portOut} handlePortState={handlePortState} />
+                        }
+                    </span>
+                </TreeHeader>
+                {expanded && fields && (
+                    <TreeBody>
+                        {
+                            fields
+                                ?.filter(f => !!f)
+                                .map((field, index) => {
+                                    return (
+                                        <InputNodeTreeItemWidget
+                                            key={index}
+                                            engine={engine}
+                                            dmType={field}
+                                            getPort={getPort}
+                                            parentId={id}
+                                            treeDepth={0}
+                                            hasHoveredParent={isHovered}
+                                            focusedInputs={focusedInputs}
+                                        />
+                                    );
+                                })
+                        }
+                    </TreeBody>
+                )}
+            </TreeContainer>
+            {expanded && dmType.convertedField &&
+                <>
+                    <ConvertWidget direction="down" />
+                    <InputNodeWidget
+                        id={dmType.convertedField.name}
+                        dmType={dmType.convertedField}
+                        engine={engine}
+                        getPort={getPort}
+                    />
+                </>
+            }
+        </>
     );
 }
