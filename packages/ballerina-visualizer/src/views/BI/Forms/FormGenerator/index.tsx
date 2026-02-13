adef550994451853236deca61a744519a41fc365
Senith Uthsara <senithkarunarathneu@gmail.com>
2026-02-13 15:15:08 +0530

    Fix type editor completions search

--- packages/ballerina-visualizer/src/views/BI/Forms/FormGenerator/index.tsx
+++ packages/ballerina-visualizer/src/views/BI/Forms/FormGenerator/index.tsx
@@ -79,6 +79,7 @@ import {
     removeDuplicateDiagnostics,
     updateLineRange,
     convertRecordTypeToCompletionItem,
+    convertItemsToCompletionItems,
 } from "../../../../utils/bi";
 import IfForm from "../IfForm";
 import { cloneDeep, debounce } from "lodash";
@@ -674,14 +675,38 @@ export const FormGenerator = forwardRef<FormExpressionEditorRef, FormProps>(func
             ) => {
                 let visibleTypes: CompletionItem[] = types;
                 if (!types.length) {
-                    const types = await rpcClient.getBIDiagramRpcClient().getVisibleTypes({
+                    const searchResponse = await rpcClient.getBIDiagramRpcClient().search({
+                        filePath: fileName,
+                        position: targetLineRange,
+                        queryMap: {
+                            q: '',
+                            offset: 0,
+                            limit: 1000
+                        },
+                        searchKind: 'TYPE'
+                    });
+
+                    const allItems = searchResponse.categories.flatMap(category => 
+                        category.items.flatMap(item => {
+                            if ('codedata' in item) {
+                                return [item];
+                            } else if ('items' in item) {
+                                return item.items;
+                            }
+                            return [];
+                        })
+                    );
+
+                    const basicTypes = await rpcClient.getBIDiagramRpcClient().getVisibleTypes({
                         filePath: fileName,
                         position: updateLineRange(targetLineRange, expressionOffsetRef.current).startLine,
                         ...(valueTypeConstraint && { typeConstraint: valueTypeConstraint })
                     });
 
                     const isFetchingTypesForDM = valueTypeConstraint === "json";
-                    visibleTypes = convertToVisibleTypes(types, isFetchingTypesForDM);
+                    const visibleSearchTypes = convertItemsToCompletionItems(allItems);
+                    const visibleBasicTypes = convertToVisibleTypes(basicTypes, isFetchingTypesForDM);
+                    visibleTypes = [...visibleSearchTypes, ...visibleBasicTypes];
                     setTypes(visibleTypes);
                 }
 
