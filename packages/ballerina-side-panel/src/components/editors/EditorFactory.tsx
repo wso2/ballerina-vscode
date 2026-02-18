d08f8afa4bd7aea6b4a14ca265ad32a85362379f
sachiniSam <sachiniprof@gmail.com>
2026-02-18 11:04:31 +0530

    Rename component name

--- packages/ballerina-side-panel/src/components/editors/EditorFactory.tsx
+++ packages/ballerina-side-panel/src/components/editors/EditorFactory.tsx
@@ -45,7 +45,7 @@ import { ActionExpressionEditor } from "./ActionExpressionEditor";
 import { CheckBoxConditionalEditor } from "./CheckBoxConditionalEditor";
 import { ActionTypeEditor } from "./ActionTypeEditor";
 import { AutoCompleteEditor } from "./AutoCompleteEditor";
-import { DependantEditor } from "./DependantEditor";
+import { DependentTypeEditor } from "./DependentTypeEditor";
 
 interface FormFieldEditorProps {
     field: FormField;
@@ -105,7 +105,7 @@ export const EditorFactory = (props: FormFieldEditorProps) => {
     if (!field.enabled || field.hidden) {
         return <></>;
     } else if (field.type === "RECORD_FIELD_SELECTOR" && field.codedata?.kind === "PARAM_FOR_TYPE_INFER") {
-        return <DependantEditor field={field} />;
+        return <DependentTypeEditor field={field} />;
     } else if (field.type === "MULTIPLE_SELECT") {
         return <MultiSelectEditor field={field} label={"Attach Another"} openSubPanel={openSubPanel} />;
     } else if (field.type === "HEADER_SET") {
