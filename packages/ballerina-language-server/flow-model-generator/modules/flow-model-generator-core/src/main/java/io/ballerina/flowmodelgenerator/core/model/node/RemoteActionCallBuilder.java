d4e87ae3dd0bc1ef36ee2717d15991237ed0e6b3
rdulmina <rdulmina@gmail.com>
2026-03-25 14:40:41 +0530

    Undo supporting `callRemoteActivity`

--- packages/ballerina-language-server/flow-model-generator/modules/flow-model-generator-core/src/main/java/io/ballerina/flowmodelgenerator/core/model/node/RemoteActionCallBuilder.java
+++ packages/ballerina-language-server/flow-model-generator/modules/flow-model-generator-core/src/main/java/io/ballerina/flowmodelgenerator/core/model/node/RemoteActionCallBuilder.java
@@ -18,19 +18,13 @@
 
 package io.ballerina.flowmodelgenerator.core.model.node;
 
-import io.ballerina.compiler.syntax.tree.ModulePartNode;
-import io.ballerina.compiler.syntax.tree.NonTerminalNode;
 import io.ballerina.compiler.syntax.tree.SyntaxKind;
-import io.ballerina.compiler.api.SemanticModel;
 import io.ballerina.flowmodelgenerator.core.model.FlowNode;
 import io.ballerina.flowmodelgenerator.core.model.NodeKind;
 import io.ballerina.flowmodelgenerator.core.model.Property;
 import io.ballerina.flowmodelgenerator.core.model.SourceBuilder;
 import io.ballerina.flowmodelgenerator.core.utils.FlowNodeUtil;
-import io.ballerina.flowmodelgenerator.core.utils.WorkflowUtil;
 import io.ballerina.modelgenerator.commons.FunctionData;
-import io.ballerina.projects.Document;
-import io.ballerina.tools.text.TextRange;
 import org.eclipse.lsp4j.TextEdit;
 
 import java.nio.file.Path;
@@ -47,38 +41,11 @@ import java.util.Set;
 public class RemoteActionCallBuilder extends CallBuilder {
 
     public static final String TARGET_TYPE_KEY = "targetType";
-    public static final String CALL_REMOTE_ACTIVITY_METHOD = "callRemoteActivity";
-
-    @Override
-    public void setConcreteTemplateData(TemplateContext context) {
-        super.setConcreteTemplateData(context);
-        Optional<Document> document = context.workspaceManager().document(context.filePath());
-        Optional<SemanticModel> model = context.workspaceManager().semanticModel(context.filePath());
-        if (document.isPresent() && model.isPresent()) {
-            int txtPos = document.get().textDocument().textPositionFrom(context.position());
-            NonTerminalNode node = ((ModulePartNode) document.get().syntaxTree().rootNode())
-                    .findNode(TextRange.from(txtPos, 0));
-            if (WorkflowUtil.isInsideWorkflowFunction(model.get(), node)) {
-                ActivityCallBuilder.addAdvanedParameters(context, moduleInfo, this);
-            }
-        }
-    }
 
     @Override
     public Map<Path, List<TextEdit>> toSource(SourceBuilder sourceBuilder) {
-        FlowNode flowNode = sourceBuilder.flowNode;
-        Map<String, Property> properties = flowNode.properties();
-        boolean isWorkflowCall = properties != null
-                && properties.containsKey(Property.ADVANCED_PARAM_KEY);
-
-        if (isWorkflowCall) {
-            return toActivityCallSource(sourceBuilder, flowNode, properties);
-        }
-        return toDefaultSource(sourceBuilder, flowNode);
-    }
-
-    private Map<Path, List<TextEdit>> toDefaultSource(SourceBuilder sourceBuilder, FlowNode flowNode) {
         sourceBuilder.newVariableWithInferredType();
+        FlowNode flowNode = sourceBuilder.flowNode;
 
         if (FlowNodeUtil.hasCheckKeyFlagSet(flowNode)) {
             sourceBuilder.token().keyword(SyntaxKind.CHECK_KEYWORD);
@@ -100,40 +67,6 @@ public class RemoteActionCallBuilder extends CallBuilder {
                 .textEdit()
                 .acceptImportWithVariableType()
                 .build();
-    }
-
-    private Map<Path, List<TextEdit>> toActivityCallSource(SourceBuilder sourceBuilder, FlowNode flowNode,
-                                                           Map<String, Property> properties) {
-        sourceBuilder.newVariable();
-
-        Optional<Property> connection = sourceBuilder.getProperty(Property.CONNECTION_KEY);
-        if (connection.isEmpty()) {
-            throw new IllegalStateException("Client must be defined for a remote activity call node");
-        }
-
-        String ctxParamName = ActivityCallBuilder.resolveContextParamName(sourceBuilder);
-        // Generate: ResultType result = check ctx->callRemoteActivity(connection, "methodName", {params...}, options);
-        sourceBuilder.token()
-                .keyword(SyntaxKind.CHECK_KEYWORD)
-                .name(ctxParamName)
-                .keyword(SyntaxKind.RIGHT_ARROW_TOKEN)
-                .name(CALL_REMOTE_ACTIVITY_METHOD)
-                .keyword(SyntaxKind.OPEN_PAREN_TOKEN)
-                .name(connection.get().toSourceCode())
-                .keyword(SyntaxKind.COMMA_TOKEN)
-                .name("\"" + flowNode.metadata().label() + "\"")
-                .keyword(SyntaxKind.COMMA_TOKEN);
-
-        Set<String> excludedKeys = Set.of(Property.VARIABLE_KEY, Property.TYPE_KEY,
-                Property.CHECK_ERROR_KEY, Property.CONNECTION_KEY, Property.ADVANCED_PARAM_KEY);
-        ActivityCallBuilder.populateActivityCallArg(sourceBuilder, properties, excludedKeys);
-        ActivityCallBuilder.populateAdvancedArgs(sourceBuilder, properties);
-
-        sourceBuilder.token()
-                .keyword(SyntaxKind.CLOSE_PAREN_TOKEN)
-                .endOfStatement();
-
-        return sourceBuilder.textEdit().build();
     }
 
     @Override
