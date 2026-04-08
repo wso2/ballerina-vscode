9c7fd21f8722aa401e5b23a395e550c739474514
Anjana S Porawagama <axewilledge123@gmail.com>
2026-04-08 16:04:08 +0530

    fix: update command execution for Windows and WSL to ensure proper quoting of paths

--- packages/ballerina-extension/src/extension.ts
+++ packages/ballerina-extension/src/extension.ts
@@ -129,13 +129,13 @@ export async function activate(context: ExtensionContext) {
     extension.context = context;
     // Init RPC Layer methods
     RPCLayer.init();
-    
+
     // Wait for the ballerina extension to be ready
     await StateMachine.initialize();
-    
+
     // Then return the ballerina extension context
-    return { 
-        ballerinaExtInstance: extension.ballerinaExtInstance, 
+    return {
+        ballerinaExtInstance: extension.ballerinaExtInstance,
         projectPath: StateMachine.context().projectPath,
         VisualizerWebview,
         BallerinaExtensionState,
