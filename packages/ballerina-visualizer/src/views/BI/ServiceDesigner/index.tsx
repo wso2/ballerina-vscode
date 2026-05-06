acb0803874285fa6093af4e2031fc969e2ad45d9
tharindulak <tharindu.jayathilake1@gmail.com>
2026-05-06 15:41:11 +0530

    Refactor AddServiceElementDropdown and introduce MoreOptionsDropdown for enhanced option management

--- packages/ballerina-visualizer/src/views/BI/ServiceDesigner/index.tsx
+++ packages/ballerina-visualizer/src/views/BI/ServiceDesigner/index.tsx
@@ -41,6 +41,7 @@ import { TitleBar } from "../../../components/TitleBar";
 import { TopNavigationBar } from "../../../components/TopNavigationBar";
 import { applyModifications, isPositionChanged } from "../../../utils/utils";
 import { AddServiceElementDropdown, DropdownOptionProps } from "./components/AddServiceElementDropdown";
+import { MoreOptionsDropdown } from "./components/MoreOptionsDropdown";
 import { ResourceAccordion } from "./components/ResourceAccordion";
 import { ResourceAccordionV2 } from "./components/ResourceAccordionV2";
 import { FunctionConfigForm } from "./Forms/FunctionConfigForm";
@@ -412,7 +413,6 @@ export function ServiceDesigner(props: ServiceDesignerProps) {
         if (service.moduleName === "http") {
             options.push({
                 title: "Export OpenAPI Spec",
-                description: "Export the OpenAPI spec for the service",
                 value: EXPORT_OAS
             });
         }
@@ -1000,9 +1000,8 @@ export function ServiceDesigner(props: ServiceDesignerProps) {
                                         )
                                     }
                                     {serviceModel && !isMcpService && dropdownOptions.length > 0 && (
-                                        <AddServiceElementDropdown
-                                            buttonTitle="More"
-                                            toolTip="More options"
+                                        <MoreOptionsDropdown
+                                            tooltip="More options"
                                             defaultOption="reusable-function"
                                             onOptionChange={handleAddDropdownOption}
                                             options={dropdownOptions}
